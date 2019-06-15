f7a76273e7f7ec526997c40cd15c549e5be147a1
Kevin Bardi <bardi.kevin@gmail.com>
2019-06-15 17:15:49 -0300

    First commit with server side rendering and sass modules

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Loadable from 'react-loadable';
+
+import indexController from './controllers/index';
+
+const PORT = 3000;
+const app = express();
+
+app.use(indexController);
+
+// start the app
+Loadable.preloadAll().then(() => {
+  app.listen(PORT, (error) => {
+    if (error) return console.log('something bad happened', error);
+    console.log(`listening on ${PORT} ...`);
+  });
+});
