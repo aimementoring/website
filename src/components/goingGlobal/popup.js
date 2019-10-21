d334055181aa54bc0a6f00e7115dda2aff13ff85
davidpaley <david.paleyy@gmail.com>
2019-10-21 19:31:27 -0300

    adding close icon to the popup

--- src/components/goingGlobal/popup.js
+++ src/components/goingGlobal/popup.js
@@ -39,7 +39,7 @@ const Popup = () => {
   }
 
   const renderModule1 = () => (
-    <div className={styles.popUp}>
+    <>
       <h4>2017</h4>
       <h1>The Golden Ticket</h1>
       <p>With our goals and dreams we tend to shoot for the moon and even 
@@ -57,27 +57,27 @@ const Popup = () => {
           title='video'
         />
       </div>
-    </div>
+    </>
   );
 
   const renderModule2 = () => (
-    <div className={styles.popUp}>
+    <>
       <h4>2017</h4>
       <h1>Going Local to go Global</h1>
       <p>Tensions were rising in Australia with some of our political leaders calling out "African Gangs" in Melbourne (hyperlink to media). We responded by reaching out and offering the model to African Australian leaders in Melbourne. </p>
       <p>We met Bem, who started the program. </p>
       <img src={`${assetsUrl}/assets/images/imagination_dec/IMAGINE-min@2x.gif`} alt="Imagine" style={{ width: '500px' }} />
-    </div>
+    </>
   );
   const renderModule3 = () => (
-    <div className={styles.popUp}>
+    <>
       <h4>2019</h4>
       <h1>USA - The Hooded Hustle</h1>
       <p>To crack the USA, we set out with big bold device, a chartered plane. We offered the chance for university students to win a Hooded Scholarship to lead AIME out of their university for the course of a year. They would also win the chance to fly to Australia and be a part of the world's first festival of mentoring. </p>
       <p>We worked with Advertising groups in New York City to share the opportunity via a national campaign, which dropped on October 1 2018. And it didn't work. </p>
       <p>We received one application.</p>
       <p>So we hit the road, some of our team from Australia drove door to door around America and the Hooded Hustle was born. We learned that we are great face to face, and we want to be the offline group that builds trust on the ground with people.</p>
-    </div>
+    </>
   );
 
   return (
@@ -104,9 +104,12 @@ const Popup = () => {
       onRequestClose={closeModal}
       style={customStyles}
     >
-      {moduleClicked === 1 && renderModule1()}
-      {moduleClicked === 2 && renderModule2()}
-      {moduleClicked === 3 && renderModule3()}
+      <div className={styles.popUp}>
+        <i className="material-icons" onClick={closeModal}>close</i>
+        {moduleClicked === 1 && renderModule1()}
+        {moduleClicked === 2 && renderModule2()}
+        {moduleClicked === 3 && renderModule3()}
+      </div>
     </Modal>
   </>
   )
