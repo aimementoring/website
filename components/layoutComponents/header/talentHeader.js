5a064cbc60ef45998998e42e869bd90439d8bc50
Kevin Bardi <kevinbardi@Kevins-MacBook-Pro-2.local>
2019-10-25 01:41:37 -0300

    Adds eslint

--- components/layoutComponents/header/talentHeader.js
+++ components/layoutComponents/header/talentHeader.js
@@ -1,27 +1,27 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import Logo from './logo'
-import TalentMobileMenu from '../mobileMenu/talentMobileMenu'
-import Anchor from '../../common/link'
-import { TALENT_HEADER_MENU_ITEMS } from '../../../constants'
-import './index.scss'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Logo from './logo';
+import TalentMobileMenu from '../mobileMenu/talentMobileMenu';
+import Anchor from '../../common/link';
+import { TALENT_HEADER_MENU_ITEMS } from '../../../constants';
+import './index.scss';
 
 const TalentHeader = ({ location, handleTalentItemClicked }) => {
-  const [showFirstHeader, setShowFirstHeader] = useState(!location.hash && !location.search)
+  const [showFirstHeader, setShowFirstHeader] = useState(!location.hash && !location.search);
 
   useEffect(() => {
-    setShowFirstHeader((!location.hash || location.hash === '#undefined') && !location.search)
-  }, [location.hash])
+    setShowFirstHeader((!location.hash || location.hash === '#undefined') && !location.search);
+  }, [location.hash]);
 
   const handleTalentItemAction = (item) => (e) => {
-    e.preventDefault()
-    handleTalentItemClicked(item)
-  }
+    e.preventDefault();
+    handleTalentItemClicked(item);
+  };
 
-  const firstHeaderClasses = classNames({ isHidden: !showFirstHeader })
-  const secondHeaderClasses = classNames({ isHidden: showFirstHeader })
-  const hash = location.hash ? location.hash.toLowerCase() : ''
+  const firstHeaderClasses = classNames({ isHidden: !showFirstHeader });
+  const secondHeaderClasses = classNames({ isHidden: showFirstHeader });
+  const hash = location.hash ? location.hash.toLowerCase() : '';
   return (
     <div className="talent-header-box">
       <div className={`flex flex-column items-center py2 ${firstHeaderClasses}`} />
@@ -31,16 +31,21 @@ const TalentHeader = ({ location, handleTalentItemClicked }) => {
         </div>
         <nav id="nav-talent" className="nav-talent nav menu-links sm-col-right ml-auto">
           <ul className="list-reset">
-            {TALENT_HEADER_MENU_ITEMS.map(headerItem => {
+            {TALENT_HEADER_MENU_ITEMS.map((headerItem) => {
               const item = headerItem.replace(' ', '');
               return (
-                <li className="inline-block relative" onClick={handleTalentItemAction(item)} key={item}>
+                <li
+                  className="inline-block relative"
+                  onClick={handleTalentItemAction(item)}
+                  key={item}
+                  onKeyPress={handleTalentItemAction(item)}
+                  role="presentation"
+                >
                   {`#${item.toLowerCase()}` === hash
-                    ? <button className="nav-btn active-item link-button">{headerItem}</button>
-                    : <button className="nav-btn link-button">{headerItem}</button>
-                  }
+                    ? <button type="button" className="nav-btn active-item link-button">{headerItem}</button>
+                    : <button type="button" className="nav-btn link-button">{headerItem}</button>}
                 </li>
-              )
+              );
             })}
             <li className="inline-block">
               <Anchor
@@ -59,12 +64,16 @@ const TalentHeader = ({ location, handleTalentItemClicked }) => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 TalentHeader.propTypes = {
-  location: PropTypes.object.isRequired,
+  location: PropTypes.shape({
+    hash: PropTypes.string,
+    search: PropTypes.string,
+    pathname: PropTypes.string,
+  }).isRequired,
   handleTalentItemClicked: PropTypes.func.isRequired,
-}
+};
 
-export default TalentHeader
+export default TalentHeader;
