0d97c8e84e0d9cf730affa7eb08df9964c3d41f4
Kevin Bardi <kevinbardi@Kevins-MacBook-Pro-2.local>
2019-10-25 01:41:37 -0300

    Adds eslint

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,14 +1,22 @@
-export const checkCountryParams = props => (
+export const checkCountryParams = (props) => (
   props.match && props.match.isExact && props.match.params
     && props.match.params.countryId
-      ? props.match.params.countryId
-      : 'global'
-)
+    ? props.match.params.countryId
+    : 'global'
+);
 
 export const sortArrayOfObjectByField = (arrayToSort, field, order = 'desc') => {
-  const orderValue = order === 'desc' ? 1 : -1
+  const orderValue = order === 'desc' ? 1 : -1;
   const sortedArray = arrayToSort.sort(
-    (a, b) => (a[field] < b[field] ? 1 : a[field] > b[field] ? -1 : 0) * orderValue
-  )
-  return sortedArray
-}
+    (a, b) => {
+      let result;
+      if (a[field] < b[field]) {
+        result = 1;
+      } else {
+        result = a[field] > b[field] ? -1 : 0;
+      }
+      return result * orderValue;
+    },
+  );
+  return sortedArray;
+};
