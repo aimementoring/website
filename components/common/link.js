5151a9f19ef37ba3203f03aefe65037ce4f3692e
Kevin Bardi <kevinbardi@Kevins-MacBook-Pro-2.local>
2019-10-24 23:19:57 -0300

    Initial commit using nextjs architecture

---
+++ components/common/link.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import PropTypes from 'prop-types'
+
+const Anchor = ({ to, as, className, children, ...props }) => (
+  <Link href={to} as={as}>
+    <a className={className} {...props}>{children}</a>
+  </Link>
+)
+
+Anchor.propTypes = {
+  to: PropTypes.string.isRequired,
+  as: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+}
+
+Anchor.defaultProps = {
+  as: null,
+  className: null,
+}
+
+export default Anchor
