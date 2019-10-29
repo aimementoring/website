84cd12438eac547a6ccaed747fe84f6c69bde493
Kevin Bardi <bardi.kevin@gmail.com>
2019-10-29 10:54:28 -0300

    Adds be-a-mentor page

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,7 +4,6 @@ import IntroPanelHomepage from '../../components/introPanelHomepage';
 import CtaGrid from '../../components/ctaGrid';
 import CtaFAQ from '../../components/ctaFAQ';
 import FooterBanner from '../../components/footerBanner';
-
 import withLayout from '../../hocs/basicLayout';
 import { CTA_AU_HOMEPAGE } from '../../constants';
 
