edb8359de2e749c98a44e130c3a174681d7305df
Kevin Bardi <bardi.kevin@gmail.com>
2019-11-11 12:41:58 -0300

    Adds ambassador page

---
+++ pages/ambassadors/ambassadorBox.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
+
+const AmbassadorBox = ({ image, name, description }) => (
+  <div className="grid-tile">
+    <div className="campaign-grid--inner-tile">
+      <img
+        className="campaign-grid--image circle mx-auto block grayscale"
+        src={`${ASSETS_URL}/assets/images/ambassadors/${image}`}
+        alt={name}
+      />
+      <h4 className="c-black campaign-grid--title">{name}</h4>
+      <p className="c-grey campaign-grid--desc">{description}</p>
+    </div>
+  </div>
+);
+
+AmbassadorBox.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default AmbassadorBox;
