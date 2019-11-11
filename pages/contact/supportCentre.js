75c770fdc4a6eae808e9aa2b4d6a13c7a0376b7e
Kevin Bardi <bardi.kevin@gmail.com>
2019-11-11 13:36:57 -0300

    Adds contact form...Pending styles with Phone input

---
+++ pages/contact/supportCentre.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Anchor from '../../components/common/link';
+
+import styles from './contact.module.scss';
+
+const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
+
+const SupportCentre = () => (
+  <div className={styles.supportCentreContainer}>
+    <img
+      className={styles.contactUsImg}
+      alt="Support Group"
+      src={`${ASSETS_URL}/assets/images/about-small.jpg`}
+    />
+    <div className={styles.supportCentreContent}>
+      <ul className={styles.supportCentreList}>
+        <h4 className={styles.title}>Support Centre</h4>
+        <p className={styles.textParagraph}>
+          {"Can't find the answer?"}
+          {' '}
+          <br />
+          {' '}
+We are here to help you.
+        </p>
+        <li className={styles.listItem}>
+          <svg className={styles.iconQuestion}>
+            <use xlinkHref="#icon-question" />
+          </svg>
+          {' '}
+          &nbsp;&nbsp;
+          <Anchor to="/faq" className={styles.link}>
+            Frequently Asked Questions
+          </Anchor>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default SupportCentre;
