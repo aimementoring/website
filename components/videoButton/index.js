b410ba67465d0d642e542ed9671dd73f644861f8
Kevin Bardi <bardi.kevin@gmail.com>
2019-11-11 11:13:42 -0300

    Fixes video button

--- components/videoButton/index.js
+++ components/videoButton/index.js
@@ -1,123 +1,132 @@
-import React, {
-  useRef, useState, useEffect,
-} from 'react';
-import ReactDOM from 'react-dom';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import videojs from 'video.js';
 import 'videojs-contrib-hls';
 import './index.scss';
 
-const VideoBox = ({ children }) => {
-  if (typeof window !== 'undefined') {
-    return ReactDOM.createPortal(
-      children,
-      document.getElementById('aime-parent-video-box'),
-    );
-  }
-  return null;
-};
+import ReactDOM from 'react-dom';
 
-const VideoButton = ({ sourceType, video, fullScreen }) => {
-  const [state, setState] = useState({
-    open: false,
-    playing: false,
-    loaded: false,
-    showContainer: false,
-  });
-
-  let player = null;
-  let parent = null;
-  const videoRef = useRef();
-  const wrapper = useRef();
-
-  const closeVideo = () => {
-    if (player) {
-      player.pause();
-      player.hide();
-    }
-    setState({ ...state, showContainer: false });
-  };
-
-  const clickAction = () => {
-    if (player && !player.isFullscreen()) {
-      player.show();
-      setState({ ...state, showContainer: true });
-      player.dimension('width', 1800);
-      if (fullScreen) player.requestFullscreen();
-      player.play();
-    }
-  };
-
-  const setupPlayer = () => {
-    if (!player) {
-      parent = wrapper.current.parentNode;
-      if (parent) {
-        parent.addEventListener('click', clickAction);
-      }
-      player = videojs(videoRef.current, {
-        autoplay: false,
-        sources: [{
-          src: video,
-          type: sourceType,
-        }],
-        controls: true,
-      }, () => {
-        if (player) {
-          player.enableTouchActivity();
-          player.hide();
-          player.on('play', () => setState({ ...state, playing: true }));
-          player.on('pause', () => setState({ ...state, playing: false }));
-          player.on('loadeddata', () => setState({ ...state, loaded: true }));
-          player.on('click', (e) => e.stopPropagation());
-          player.on('fullscreenchange', () => {
-            if (!player.isFullscreen()) closeVideo();
-          });
-        }
-      });
-    }
-  };
-
-  useEffect(() => {
-    setupPlayer();
-
-    const cleanup = () => {
-      if (parent) parent.removeEventListener('click', clickAction);
-      if (player) player.dispose();
+const VideoBox = ({ children }) => ReactDOM.createPortal(children, document.getElementById('aime-parent-video-box'));
+class VideoButton extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false,
+      loaded: false,
+      didMount: false,
+      showContainer: false,
     };
-    return cleanup;
-  }, []);
+  }
 
-  useEffect(() => {
-    setupPlayer();
-  }, [player]);
+  componentDidMount() {
+    this.setState({ didMount: true });
+  }
 
-  return (
-    <>
+  componentDidUpdate() {
+    const { didMount } = this.state;
+    if (didMount && !this.player) {
+      const { sourceType, video, fullScreen } = this.props;
+      this.parent = this.wrapper.parentNode;
+      if (this.parent) {
+        this.parent.addEventListener('click', () => {
+          if (this.player && !this.player.isFullscreen()) {
+            this.player.show();
+            this.setState({ showContainer: true });
+            this.player.dimension('width', 1800);
+            if (fullScreen) this.player.requestFullscreen();
+            this.player.play();
+          }
+        });
+      }
+      this.player = videojs(
+        this.video,
+        {
+          autoplay: false,
+          sources: [
+            {
+              src: video,
+              type: sourceType,
+            },
+          ],
+          controls: true,
+        },
+        () => {
+          if (this.player) {
+            this.player.enableTouchActivity();
+            this.player.hide();
+            this.player.on('play', () => {
+              this.setState({ playing: true });
+            });
+            this.player.on('pause', () => {
+              this.setState({ playing: false });
+            });
+            this.player.on('loadeddata', () => {
+              this.setState({ loaded: true });
+            });
+            this.player.on('fullscreenchange', () => {
+              if (!this.player.isFullscreen()) {
+                this.closeVideo();
+              }
+            });
+            this.player.on('click', (e) => {
+              e.stopPropagation();
+            });
+          }
+        },
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.parent) this.parent.removeEventListener('click', this.click);
+    if (this.player) this.player.dispose();
+  }
+
+  closeVideo = () => {
+    if (this.player) {
+      this.player.pause();
+      this.player.hide();
+    }
+    this.setState({ showContainer: false });
+  };
+
+  render() {
+    const {
+      playing, loaded, showContainer, didMount,
+    } = this.state;
+
+    return (
       <div
         data-vjs-player
-        ref={wrapper}
-        style={{ visibility: state.loaded ? 'visible' : 'hidden' }}
+        ref={(c) => { this.wrapper = c; }}
+        style={{ visibility: loaded ? 'visible' : 'hidden' }}
       >
-        <VideoBox>
-          <div
-            className={`container-video ${!state.showContainer && 'display-none'}`}
-            onClick={closeVideo}
-            onKeyPress={closeVideo}
-            role="presentation"
-          >
-            <svg className="icon icon-close">
-              <use xlinkHref="#icon-close" />
-            </svg>
-            <video className="video-js" ref={videoRef} playsInline>
-              <track default srcLang="en" kind="captions" />
-            </video>
-            {!state.playing && <div className="video-arrow">▶</div>}
-          </div>
-        </VideoBox>
+        {didMount && (
+          <VideoBox>
+            <div
+              className={`container-video ${!showContainer && 'display-none'}`}
+              onClick={this.closeVideo}
+              onKeyPress={this.closeVideo}
+              role="presentation"
+            >
+              <svg className="icon icon-close">
+                <use xlinkHref="#icon-close" />
+              </svg>
+              <video
+                className="video-js"
+                ref={(node) => { this.video = node; }}
+                playsInline
+              >
+                <track kind="captions" />
+              </video>
+              {!playing && <div className="video-arrow">▶</div>}
+            </div>
+          </VideoBox>
+        )}
       </div>
-    </>
-  );
-};
+    );
+  }
+}
 
 VideoButton.propTypes = {
   sourceType: PropTypes.string,
