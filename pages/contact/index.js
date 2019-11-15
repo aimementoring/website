c3faa4f01e8f6f28b7e9c5f3f5fbf5506bc1ba69
Kevin Bardi <bardi.kevin@gmail.com>
2019-11-15 11:50:25 -0300

    Adds going global and fixes layout

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ContactBanner from './contactBanner';
 import SupportCentre from './supportCentre';
 import ContactForm from './contactForm';
-import withLayout from '../../hocs/basicLayout';
+import Layout from '../../hocs/basicLayout';
 
 import styles from './contact.module.scss';
 
 const Contact = () => (
-  <>
+  <Layout>
     <ContactBanner />
     <section className={styles.contactSection}>
       <div className={styles.contactSectionWrapper}>
@@ -15,7 +15,7 @@ const Contact = () => (
         <ContactForm />
       </div>
     </section>
-  </>
+  </Layout>
 );
 
-export default withLayout(Contact);
+export default Contact;
