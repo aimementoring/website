c18ae00feefad75902f9e935cc294a6458b39694
Kevin Bardi <bardi.kevin@gmail.com>
2019-11-15 16:55:48 -0300

    Refactor and some improvements

--- components/universitySelector/index.js
+++ components/universitySelector/index.js
@@ -1,40 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { loadUniversities } from '../../services/positions';
 
 const UniversitySelector = ({
-  placeholder, classNames, containerClassNames, onChangeFunction,
-}) => {
-  const [universities, setUniversities] = useState([]);
+  placeholder, classNames, containerClassNames, onChangeFunction, universities,
+}) => (
+  <div className={containerClassNames}>
+    <select
+      placeholder={placeholder}
+      name="uni-campus-attending"
+      className={classNames}
+      onChange={onChangeFunction}
+      defaultValue=""
+      required
+    >
+      <option value="" disabled>
+        {placeholder}
+      </option>
+      {universities.length
+        && universities
+          .filter((university) => university.text && university.value)
+          .map((university) => (
+            <option key={university.value} disabled="" value={university.value}>
+              {university.text}
+            </option>
+          ))}
+    </select>
+  </div>
+);
 
-  useEffect(() => {
-    loadUniversities().then((value) => setUniversities(value));
-  }, []);
-
-  return (
-    <div className={containerClassNames}>
-      <select
-        placeholder={placeholder}
-        name="uni-campus-attending"
-        className={classNames}
-        onChange={onChangeFunction}
-        defaultValue=""
-        required
-      >
-        <option value="" disabled>
-          {placeholder}
-        </option>
-        {universities.length
-          && universities
-            .filter((university) => university.text && university.value)
-            .map((university) => (
-              <option key={university.value} disabled="" value={university.value}>
-                {university.text}
-              </option>
-            ))}
-      </select>
-    </div>
-  );
+UniversitySelector.getInitialProps = async () => {
+  const universities = await loadUniversities();
+  return { universities };
 };
 
 UniversitySelector.propTypes = {
@@ -42,6 +39,7 @@ UniversitySelector.propTypes = {
   classNames: PropTypes.string,
   containerClassNames: PropTypes.string,
   onChangeFunction: PropTypes.func,
+  universities: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 UniversitySelector.defaultProps = {
