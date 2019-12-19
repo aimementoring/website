6e7783a0cdbf39b1bbfca11e684cea3ccd9f8884
Lara Salameh <hello@larasalameh.com>
2019-12-19 17:02:50 +1100

    home ambassador carousel updates

--- components/ambassadors/index.js
+++ components/ambassadors/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import classNames from 'classnames';
 import dynamic from 'next/dynamic';
+import { Components } from 'aime-blueprint';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import Anchor from '../common/link';
 import styles from './ambassadors.module.scss';
 
 const Carousel = dynamic(() => import(/* webpackChunkName 'Carousel' */ '../carousel'));
 
+const {
+  Title,
+  Button,
+} = Components;
+
 const Ambassadors = () => (
   <div className={styles.mainContainer}>
     <div className={styles.mentorsSection}>
       <div className={styles.mentorsSectionWrapper}>
-        <h1>
+        {/* <Title>
           <span className={styles.preText}>Global</span>
           <span className={styles.gradientText}>Ambassadors</span>
-        </h1>
+        </Title> */}
+        <Title type="headingLockup">
+          Global
+          <strong>Ambassadors</strong>
+        </Title>
         <div className={styles.carousel}>
           <Carousel type="ambassadors">
             <div className={styles.mentorSlideWrapper}>
@@ -25,15 +36,15 @@ const Ambassadors = () => (
               <div className={styles.mentorContent}>
                 <div className={styles.mentorContentWrapper}>
                   <div className={styles.titleContainer}>
-                    <h2>Uzo Aduba</h2>
-                    <h3 className={styles.mainTitle}>Ambassador</h3>
-                    <p className={styles.paragraph}>
+                    <Title type="h2Title" className={styles.ambassadorName}>Uzo Aduba</Title>
+                    <Title type="h5Title">Ambassador</Title>
+                    <Paragraph>
                       By standing together with dignity and hope for the future, AIME transforms
                       lives.
-                    </p>
-                    <Anchor to="/ambassadors" className={styles.linkButton}>
+                    </Paragraph>
+                    <Button theme="rainbow" aria-label="cta" type="link" url="/ambassadors">
                       See more ambassadors
-                    </Anchor>
+                    </Button>
                   </div>
                 </div>
               </div>
@@ -50,18 +61,17 @@ const Ambassadors = () => (
               <div className={styles.mentorContent}>
                 <div className={styles.mentorContentWrapper}>
                   <div className={styles.titleContainer}>
-                    <h2>Patrick J. Adams</h2>
-                    <h3 className={styles.mainTitle}>Ambassador</h3>
-                    <p className={styles.paragraph}>
+                    <Title type="h2Title" className={styles.ambassadorName}>Patrick J. Adams</Title>
+                    <Title type="h5Title">Ambassador</Title>
+                    <Paragraph>
                       Beyond my family, no people have helped shape my life, my career and my view
                       of the world more than the mentors I have been lucky enough to work, play and
                       live with. I’m so excited to have found AIME and to do everything I can to
                       serve the cause of mentorship all over the world.
-                      {' '}
-                    </p>
-                    <Anchor to="/ambassadors" className={styles.linkButton}>
+                    </Paragraph>
+                    <Button theme="rainbow" aria-label="cta" type="link" url="/ambassadors">
                       See more ambassadors
-                    </Anchor>
+                    </Button>
                   </div>
                 </div>
               </div>
@@ -78,16 +88,16 @@ const Ambassadors = () => (
               <div className={styles.mentorContent}>
                 <div className={styles.mentorContentWrapper}>
                   <div className={styles.titleContainer}>
-                    <h2>Ian Thorpe</h2>
-                    <h3 className={styles.mainTitle}>Ambassador</h3>
-                    <p className={styles.paragraph}>
+                    <Title type="h2Title" className={styles.ambassadorName}>Ian Thorpe</Title>
+                    <Title type="h5Title">Ambassador</Title>
+                    <Paragraph>
                       I believe in the work AIME does and I want to see it broaden to being a
                       service globally. I want to see this happen because I believe in the young
                       people of our future and the future they can create for the world.
-                    </p>
-                    <Anchor to="/ambassadors" className={styles.linkButton}>
+                    </Paragraph>
+                    <Button theme="rainbow" aria-label="cta" type="link" url="/ambassadors">
                       See more ambassadors
-                    </Anchor>
+                    </Button>
                   </div>
                 </div>
               </div>
