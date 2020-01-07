fe5bebd5b7128d1e914ea126d686ca04ce43fa9a
Rory <38899587+bronz3beard@users.noreply.github.com>
2020-01-07 13:42:00 +1000

    added intercom button to nav header (#63)
    * added intercom button to nav header
    
    * removed contact page
    
    * remove comments from server.js

--- api/contentfulServer.js
+++ api/contentfulServer.js
@@ -1,27 +1,30 @@
 const contentful = require('contentful');
 // const bugsnagClient = require('../utils/bugsnag');
 
-const {
-  REACT_APP_CONTENTFUL_SPACE_ID,
-  REACT_APP_CONTENTFUL_CONTENT_TYPE,
-  REACT_APP_CONTENTFUL_ENTRY_KEY,
-  REACT_APP_CONTENTFUL_API_KEY,
-} = process.env;
-
 const client = contentful.createClient({
-  space: REACT_APP_CONTENTFUL_SPACE_ID,
-  entry_id: REACT_APP_CONTENTFUL_ENTRY_KEY,
-  accessToken: REACT_APP_CONTENTFUL_API_KEY,
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.REACT_APP_CONTENTFUL_API_ENVIRONMENT === 'staging'
+    ? process.env.REACT_APP_CONTENTFUL_API_KEY_STAGING
+    : process.env.REACT_APP_CONTENTFUL_API_KEY_MASTER,
+  entry_id: process.env.REACT_APP_CONTENTFUL_API_ENVIRONMENT === 'staging'
+    ? process.env.REACT_APP_CONTENTFUL_ENTRY_KEY_STORIES
+    : process.env.REACT_APP_CONTENTFUL_ENTRY_KEY_REDIRECTS,
+  environment: process.env.REACT_APP_CONTENTFUL_API_ENVIRONMENT === 'staging'
+    ? process.env.REACT_APP_CONTENTFUL_API_ENVIRONMENT
+    : 'master',
 });
 
 module.exports = () => (
   client
     .getEntries({
-      content_type: REACT_APP_CONTENTFUL_CONTENT_TYPE,
+      content_type: process.env.REACT_APP_CONTENTFUL_API_ENVIRONMENT === 'staging'
+        ? process.env.REACT_APP_CONTENTFUL_CONTENT_TYPE_STORIES
+        : process.env.REACT_APP_CONTENTFUL_CONTENT_TYPE_REDIRECTS,
       include: 2,
     })
     .then((response) => response.items)
     .catch((error) => {
+      // eslint-disable-next-line no-console
       console.log(error);
       // bugsnagClient.notify(
       //   new Error(`There was an issue fetching content from Contentful ${error}`),
