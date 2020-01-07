161ba7eff77d377552885016772ca3edfbb7eba7
Rory <38899587+bronz3beard@users.noreply.github.com>
2020-01-07 13:56:44 +1000

    Feature/aime stories contentful #34 (#40)
    * WIP Added contentful to stories, updated carousel
    
    * fixed routing to stories page
    
    * updated utilities
    
    * VS code removed files need to restore what i can.
    
    * WIP added contentcard take 2
    
    * small code refactoring fixing story title
    
    * add comments, storiescarousel comp updated styles
    
    * updated cdnRoot to .net, fixed link carousel style
    
    * adding node version to package json.
    
    * Fixes node version on package json
    
    * update to reflect pr review feedback
    
    * adjusted node version package.json
    
    * added aws  directory and files
    
    * updated aws path, scripts and added credentials
    
    * update node version package.json
    
    * updated scripts package.json
    
    * updated node version again in package.json
    
    * testing travis build
    
    * testing travis build
    
    * fixed story page refresh issue
    
    * change node version in package.json
    
    * stories sorted by publishDate updated video comp
    
    * update node version to match travis
    
    * added cli migration scripts & folder 4 contentful
    
    * updated all env variables for contentful
    
    * changed node version in package.json
    
    * WIP breaking change contentful migration scripts
    
    * fixed breaking changes
    
    * added new migration scripts
    
    * update node version for travis
    
    * update node version for travis
    
    * added react-md, migration script, fix story styles
    
    * changed node version
    
    * updated yarn.lock
    
    * update node version to match travis
    
    * fixed visualmedia checks, updated node
    
    * changed node to keep travis happy
    
    * added image caption, signature
    
    * update node version, add ps to story page content
    
    * update story comps dir add story button and styles
    
    * remove lighthouse-bot from travis
    
    * fix buttonProps proptype default
    
    * update vidoe component
    
    Co-authored-by: Kevin Bardi <bardi.kevin@gmail.com>

---
+++ components/storiesComponents/storiesContentTwo/index.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import dynamic from 'next/dynamic';
+import { formatDate } from '../../../utils/utilities';
+import Anchor from '../../common/link';
+import styles from './storiesContentTwo.module.scss';
+
+const Picture = dynamic(() => import('../../picture'));
+
+const StoriesContentTwo = (props) => {
+  const {
+    id,
+    title,
+    slugTitle,
+    bannerContent,
+    publishDate,
+    contentCards,
+    contentCreator,
+    contentPreview,
+  } = props;
+
+  const datePublished = formatDate(publishDate, 'short');
+  const bannerImage = bannerContent.visualMedia
+  && bannerContent.visualMedia.fields
+  && bannerContent.visualMedia.fields.file.url;
+
+  return (
+    <article key={`article-story-${id}`} className={styles.articleTile}>
+      <Anchor
+        to={`/storyTwo?slug=${slugTitle}`}
+        as={`/storyTwo/${slugTitle}`}
+        className={styles.articleLink}
+      >
+        <div>
+          <Picture
+            className={styles.bannerImage}
+            thumbnail
+            image={{
+              image: `https:${bannerImage}`,
+              title,
+              thumbnail: `https:${bannerImage}`,
+            }}
+          />
+          <div
+            key={`article-description-${id}`}
+            className={styles.articleDescription}
+          >
+            <h1 className="article-tile-title">{title}</h1>
+            <p className="article-tile-tagline">
+              <span key={`pr1-story-entry-${id}`} className={styles.postDate}>
+                {datePublished}
+              </span>
+              <span key={`c-light-grey-span-${id}`} className={styles.slash}>
+                        /
+              </span>
+              <span key={`px1-span-${id}`} className={styles.author}>
+                {`By ${contentCreator}`}
+              </span>
+            </p>
+
+            <p className={styles.articleTileLabel}>
+              {contentPreview && contentPreview.previewCopy ? (
+                `${contentPreview.previewCopy.slice(0, 230)}...`
+              )
+                : contentCards && contentCards.slice(0, 1).map((card) => (
+                  card.fields.contentCopy
+                    && (
+                      `${card.fields.contentCopy.slice(0, 230).replace(/[*_>]*/g, '')}...`
+                    )))}
+            </p>
+            <div className={styles.articleTileLink}>Read more</div>
+          </div>
+        </div>
+      </Anchor>
+    </article>
+  );
+};
+
+StoriesContentTwo.defaultProps = {
+  contentCards: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+StoriesContentTwo.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  slugTitle: PropTypes.string,
+  publishDate: PropTypes.string,
+  contentCreator: PropTypes.string,
+  bannerContent: PropTypes.shape({
+    displayType: PropTypes.string,
+    heading: PropTypes.shape({
+      headingText: PropTypes.string,
+      type: PropTypes.string,
+    }),
+    visualMedia: PropTypes.shape({
+      fields: PropTypes.shape({
+        description: PropTypes.string,
+        fileName: PropTypes.string,
+        title: PropTypes.string,
+        file: PropTypes.shape({
+          contentType: PropTypes.string,
+          details: PropTypes.shape({
+            size: PropTypes.number,
+            image: PropTypes.shape({
+              height: PropTypes.number,
+              width: PropTypes.number,
+            }),
+          }),
+          fileName: PropTypes.string,
+          url: PropTypes.string.isRequired,
+          title: PropTypes.string,
+        }),
+      }),
+    }),
+  }).isRequired,
+  contentCards: PropTypes.arrayOf(PropTypes.shape({
+    Type: PropTypes.string,
+    contentCopy: PropTypes.string,
+    displayType: PropTypes.string,
+    visualMedia: PropTypes.shape({
+      file: PropTypes.shape({
+        contentType: PropTypes.string,
+        fileName: PropTypes.string,
+        url: PropTypes.string,
+        title: PropTypes.string,
+        details: PropTypes.shape({
+          size: PropTypes.number,
+          image: PropTypes.shape({
+            height: PropTypes.number,
+            width: PropTypes.number,
+          }),
+        }),
+      }),
+    }),
+    visualMediaCarousel: PropTypes.arrayOf(PropTypes.shape({
+
+    })),
+  })),
+  contentPreview: PropTypes.shape({
+    Type: PropTypes.string,
+    previewCopy: PropTypes.string,
+    displayType: PropTypes.string,
+    visualMedia: PropTypes.shape({
+      file: PropTypes.shape({
+        contentType: PropTypes.string,
+        fileName: PropTypes.string,
+        url: PropTypes.string,
+        title: PropTypes.string,
+        details: PropTypes.shape({
+          size: PropTypes.number,
+          image: PropTypes.shape({
+            height: PropTypes.number,
+            width: PropTypes.number,
+          }),
+        }),
+      }),
+    }),
+    visualMediaCarousel: PropTypes.arrayOf(PropTypes.shape({})),
+  }),
+};
+
+StoriesContentTwo.defaultProps = {
+  slugTitle: '',
+  publishDate: '',
+  contentCreator: '',
+  contentPreview: null,
+};
+
+export default StoriesContentTwo;
