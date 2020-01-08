6081deb0d4b643171e0b41326e657e637c865150
Kevin Bardi <bardi.kevin@gmail.com>
2020-01-08 18:33:39 -0300

    Improve styles and convert all homepage components to css modules

--- components/heroBannerHomepage/index.js
+++ components/heroBannerHomepage/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import dynamic from 'next/dynamic';
 
-const HeroBannerAustralia = dynamic(() => import(/* webpackChunkName 'HeroBannerAustralia' */ './heroBannerAustralia'));
-const HeroBannerUSA = dynamic(() => import(/* webpackChunkName 'HeroBannerUSA' */ './heroBannerUSA'));
+const HeroBannerAustralia = dynamic(() => import('./heroBannerAustralia'));
+const HeroBannerUSA = dynamic(() => import('./heroBannerUSA'));
 
 const HeroBannerHomepage = ({ currentSite }) => (
   currentSite === 'us' || currentSite === 'global' ? <HeroBannerUSA /> : <HeroBannerAustralia />
