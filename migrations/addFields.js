c9d9737821a497c83a71238dbfd1c162887409fb
Rory <38899587+bronz3beard@users.noreply.github.com>
2020-01-15 13:22:39 +1000

    Remove Craft/stories and replace with Contentful/stories (#80)
    * removed Craft/stories updated Contentful/stories
    
    * removed Craft/stories from server
    
    * changes made based on review
    
    * added style changes as per review

--- migrations/addFields.js
+++ migrations/addFields.js
@@ -1,12 +1,3 @@
-// import { getEntries } from '../services/craftAPI';
-/*
-const fetchStories = async () => {
-  const { data } = await getEntries('stories');
-  return data && data.map((entry) => (entry));
-};
-*/
-// const entry = fetchStories();
-
 module.exports = (migration) => {
   // currnt Types are, 'post' and 'redirectEntries'
   const post = migration.editContentType('post');
@@ -17,24 +8,4 @@ module.exports = (migration) => {
 
 
   post.moveField('craftTitle').toTheTop();
-  /* post.createField('sugar')
-      .type('Number')
-      .name('Amount of sugar');
-
-    post.createField('vegan')
-      .type('Boolean')
-      .name('Vegan friendly');
-
-    post.createField('producer')
-      .type('Symbol')
-      .name('post producer');
-
-    post.createField('gmo')
-      .type('Boolean')
-      .name('Genetically modified post'); */
-
-
-  /* post.moveField('sugar').toTheBottom();
-    post.moveField('producer').beforeField('vegan');
-    post.moveField('gmo').afterField('vegan'); */
 };
