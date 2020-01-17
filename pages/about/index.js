51588a2d375a03ac4dc42330ed0c5f9417321ab4
Lara Salameh <hello@larasalameh.com>
2020-01-17 14:31:30 +1100

    replacing about markup w bp components

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import classNames from 'classnames';
 import dynamic from 'next/dynamic';
 import { Components } from 'aime-blueprint';
 import Paragraph from 'aime-blueprint/lib/components/paragraph';
-import Anchor from '../../components/common/link';
 import Layout from '../../hocs/basicLayout';
 import styles from './about.module.scss';
 
@@ -26,21 +24,24 @@ const About = () => (
             About
             <strong>AIME</strong>
           </Title>
-          <Paragraph>
-            <strong>
-              We are driven to unlock the limitless potential of children who have been left behind.
-            </strong>
-          </Paragraph>
-          <Paragraph>
-            We do that by building mentoring bridges between universities and high schools,
-            between the powerful and the powerless, the haves and the have nots. We know that
-            our fiery and intuitive brand of mentoring ends the cycle of disadvantage by
-            permanently changing mindsets. Based in Redfern, Australia, our operation runs
-            across campuses worldwide.
-          </Paragraph>
-          <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="/know-aime">
-            Tell me more
-          </Button>
+          <div className={styles.copy}>
+            <Paragraph>
+              <strong>
+                We are driven to unlock the limitless potential of children who have
+                been left behind.
+              </strong>
+            </Paragraph>
+            <Paragraph>
+              We do that by building mentoring bridges between universities and high schools,
+              between the powerful and the powerless, the haves and the have nots. We know that
+              our fiery and intuitive brand of mentoring ends the cycle of disadvantage by
+              permanently changing mindsets. Based in Redfern, Australia, our operation runs
+              across campuses worldwide.
+            </Paragraph>
+            <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="/know-aime">
+              Tell me more
+            </Button>
+          </div>
           {/* <div className={classNames(styles.titleLockup, styles.leftWithMargin)}>
             <h1>
               <span className={styles.leftHighlightText}>
@@ -81,21 +82,23 @@ const About = () => (
       </section>
       <section className={styles.sectionAbout}>
         <div className={styles.column}>
-          <div className={classNames(styles.titleLockup, styles.left)}>
-            <h1>
-              <span className={styles.gradientTitle}>The Kids</span>
-              <span className={styles.postTextRight}>The kids at AIME</span>
-            </h1>
-          </div>
+          <Title type="headingLockup" className={styles.sectionHeading} theme={process.env.REACT_APP_THEME}>
+            The
+            <strong>Kids</strong>
+            At AIME
+          </Title>
           <div className={styles.copy}>
-            <h3 className={styles.whiteTitleWithMargin}>Providing the stage</h3>
-            <p>
+            <Paragraph>
+              <strong>
+                Providing the stage.
+              </strong>
+            </Paragraph>
+            <Paragraph>
               Everything we do is about these kids. They see themselves as being on the outside
               looking in…. that’s until we give them the mic. AIME program days are all about
               that. The Kids at AIME are central to the entire organisation. We’re just
               providing the framework, but they’re making the difference.
-              {' '}
-            </p>
+            </Paragraph>
           </div>
         </div>
         <div className={styles.column}>
@@ -108,22 +111,25 @@ const About = () => (
       </section>
       <section className={styles.sectionAbout}>
         <div className={styles.column}>
-          <div className={classNames(styles.titleLockup, styles.left)}>
-            <h1>
-              <span className={styles.gradientTitle}>Impact</span>
-              <span className={styles.postTextLeft}>Hard lined impact</span>
-            </h1>
-          </div>
+          <Title type="headingLockup" className={styles.sectionHeading} theme={process.env.REACT_APP_THEME}>
+            Hard lined
+            <strong>Impact</strong>
+          </Title>
           <div className={styles.copy}>
-            <h3 className={styles.whiteTitleWithMargin}>Not another smiley faced program</h3>
-            <p>
-              {`We turn up. Turn up. Turn up. We've been at it since 2005 and we're only just
-              getting started! AIME benefits mentees, mentors and the entire community we work
-              in. Don't take our word for it, check out the evidence.`}
-            </p>
-            <Anchor to="/impact" className={styles.linkButton}>
+            <Paragraph>
+              <strong>
+                Not another smiley faced program.
+              </strong>
+            </Paragraph>
+            <Paragraph>
+              We turn up. Turn up. Turn up. We&apos;ve been at it since 2005
+              and we&apos;re only just getting started! AIME benefits mentees,
+              mentors and the entire community we work
+              in. Don&apos;t take our word for it, check out the evidence.
+            </Paragraph>
+            <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="/impact">
               Read more
-            </Anchor>
+            </Button>
           </div>
         </div>
         <div className={styles.column}>
@@ -136,25 +142,24 @@ const About = () => (
       </section>
       <section className={styles.sectionAbout}>
         <div className={styles.column}>
-          <div className={classNames(styles.titleLockup, styles.left)}>
-            <h1>
-              <span className={styles.gradientTitle}>The gift</span>
-              <span className={styles.postTextRight}>The gift of knowledge</span>
-            </h1>
-          </div>
+          <Title type="headingLockup" className={styles.sectionHeading} theme={process.env.REACT_APP_THEME}>
+            The gift of
+            <strong>Knowledge</strong>
+          </Title>
           <div className={styles.copy}>
-
-            <h3 className={styles.whiteTitleWithMargin}>
-              {'We\'ve got knowledge & tools to share'}
-            </h3>
-            <p>
+            <Paragraph>
+              <strong>
+                We&apos;ve got knowledge & tools to share.
+              </strong>
+            </Paragraph>
+            <Paragraph>
               In 2018 we opened our model on demand to Universities from across the globe, with
               plans to be operational in 10+ different countries in 2020. Checkout our knowledge
               and tools and how you can get involved.
-            </p>
-            <Anchor to="/know-aime" className={styles.linkButton}>
-              Learn more
-            </Anchor>
+            </Paragraph>
+            <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="/know-aime">
+            Learn more
+            </Button>
           </div>
         </div>
         <div className={styles.column}>
@@ -167,28 +172,26 @@ const About = () => (
       </section>
       <section className={styles.sectionAbout}>
         <div className={styles.column}>
-          <div className={classNames(styles.titleLockup, styles.left)}>
-            <h1>
-              <span className={styles.gradientTitle}>Team</span>
-              <span className={styles.postTextLeft}>Our team</span>
-            </h1>
-          </div>
+          <Title type="headingLockup" className={styles.sectionHeading} theme={process.env.REACT_APP_THEME}>
+            Our
+            <strong>Team</strong>
+          </Title>
           <div className={styles.copy}>
-            <h3 className={styles.whiteTitleWithMargin}>Rebels with a cause</h3>
-            <p>
+            <Paragraph>
+              <strong>
+                Rebels with a cause.
+              </strong>
+            </Paragraph>
+            <Paragraph>
               We are a small band of artists at AIME, writing, shaping, creating new worlds,
               challenging est world orders, making magic, fueling imaginative fire.
-            </p>
-            <Anchor
-              to="/founder"
-              as="/jack-manning-bancroft"
-              className={styles.linkButton}
-            >
-              Read about our founder
-            </Anchor>
-            <Anchor to="/positions" className={styles.positionsButton}>
-              Join our team
-            </Anchor>
+            </Paragraph>
+            <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="/founder">
+            Read about our founder
+            </Button>
+            <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="/positions">
+            Join our team
+            </Button>
           </div>
         </div>
         <div className={styles.column}>
@@ -201,32 +204,26 @@ const About = () => (
       </section>
       <section className={styles.sectionAbout}>
         <div className={styles.column}>
-          <div className={styles.titleLockup}>
-            <h1>
-              <span className={styles.gradientTitle}>Apparel</span>
-              <span className={styles.postTextRight}>Apparel that changes lives</span>
-            </h1>
-          </div>
+          <Title type="headingLockup" className={styles.sectionHeading} theme={process.env.REACT_APP_THEME}>
+            <strong>Apparel</strong>
+            That changes lives
+          </Title>
           <div className={styles.copy}>
-            <h3 className={styles.whiteTitleWithMargin}>
-              AIME makes the greatest hoodie on earth
-            </h3>
-            <p>
+            <Paragraph>
+              <strong>
+                AIME makes the greatest hoodie on earth.
+              </strong>
+            </Paragraph>
+            <Paragraph>
               We are doing our best to make the most meaningful hoodie on the planet, check out
               the range designed and inspired by kids to change the world. Our shop currently
               ships out of Australia, if you can’t wait to get your hands on it, it usually
               takes a few weeks to send over. And jump onto our Saturday Swag list to be the
               first to know when AIME Apparel launches into the US in a legit way in 2019.
-            </p>
-            <a
-              href="https://shop.aimementoring.com/collections/hoodies/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.linkButton}
-              aria-label="shop apparel"
-            >
-              Shop Apparel
-            </a>
+            </Paragraph>
+            <Button theme={process.env.REACT_APP_THEME} aria-label="cta" type="link" url="https://shop.aimementoring.com/">
+            Shop Apparel
+            </Button>
           </div>
         </div>
         <div className={styles.column}>
