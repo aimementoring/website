e3496f575cd0e28d130bd753a6f2775de4d1587f
Lara Salameh <hello@larasalameh.com>
2020-01-20 20:00:56 +1100

    WIP be a mentor updates

--- pages/beAMentor/universityLinks.js
+++ pages/beAMentor/universityLinks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './beAMentor.scss';
+import './beAMentor.module.scss';
 
 const UniversityLinks = ({ options }) => (
   <div className="display-none">
