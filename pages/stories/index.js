1bab28d297f5c1205e751ea1476f72d4c8ebdc27
Rory <38899587+bronz3beard@users.noreply.github.com>
2020-01-20 11:45:13 +1000

    Updated all envs for Contentful master environment (#89)
    * updated all envs, added redirect call, all from master in contentful
    
    * removed comment

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from '../../hocs/basicLayout';
-import contentfulServer from '../../api/contentfulServer';
+import contentfulServer from '../../api/contentfulStories';
 import {
   removeSpecialCharacters,
   replaceWhiteSpace,
