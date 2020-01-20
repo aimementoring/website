97338c2d4178bdfbd1608d46329e49e9a87b0d8c
Lara Salameh <hello@larasalameh.com>
2020-01-20 19:42:58 +1100

    WIP be a mentor updates

--- pages/beAMentor/heroBanner.js
+++ pages/beAMentor/heroBanner.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './beAMentor.scss';
+import Title from 'aime-blueprint/lib/components/title';
+import styles from './beAMentor.module.scss';
 
 const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 const imgStyle = {
@@ -11,7 +12,11 @@ const HeroBanner = () => (
   <div className="hero-banner--default hero-banner--history full-width-wrap" style={imgStyle}>
     <div className="flex flex-wrap items-center w100 h100">
       <div className="banner-wrapper subpage-banner center">
-        <h1>
+        <Title type="headingLockup" className={styles.bannerHeading} theme={process.env.REACT_APP_THEME}>
+          You can be a
+          <strong>mentor</strong>
+        </Title>
+        {/* <h1>
           <span className="pre-text">You can be a</span>
           <span className="highlight-text">
             <em className="border-gradient">
@@ -22,7 +27,7 @@ const HeroBanner = () => (
           <p className="mt2 pt1 col-8 block mx-auto left-align">
             AIME is always looking for exceptional humans to step up and be the change
           </p>
-        </h1>
+        </h1> */}
       </div>
     </div>
   </div>
