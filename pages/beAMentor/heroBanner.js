035a42a528e635e68e3eb69654c88e5a786d4c38
Lara Salameh <hello@larasalameh.com>
2020-01-21 16:50:35 +1100

    updated env assetsurl and beamentor 2020

--- pages/beAMentor/heroBanner.js
+++ pages/beAMentor/heroBanner.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import Title from 'aime-blueprint/lib/components/title';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import styles from './beAMentor.module.scss';
 
-const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
-const imgStyle = {
-  backgroundImage: `url('${ASSETS_URL}/assets/images/banner/Bianca@2x-min.jpg')`,
-  backgroundSize: 'cover',
-};
-
 const HeroBanner = () => (
-  <div className="hero-banner--default hero-banner--history full-width-wrap" style={imgStyle}>
+  <div className="hero-banner--default hero-banner--mentor full-width-wrap">
     <div className="flex flex-wrap items-center w100 h100">
       <div className="banner-wrapper subpage-banner center">
         <Title type="headingLockup" className={styles.bannerHeading} theme={process.env.REACT_APP_THEME}>
-          You can be a
+          Become a
           <strong>mentor</strong>
         </Title>
-        {/* <h1>
-          <span className="pre-text">You can be a</span>
-          <span className="highlight-text">
-            <em className="border-gradient">
-              Mentor
-              <br />
-            </em>
-          </span>
-          <p className="mt2 pt1 col-8 block mx-auto left-align">
-            AIME is always looking for exceptional humans to step up and be the change
-          </p>
-        </h1> */}
+        <Paragraph>Welcome to the world of mentoring</Paragraph>
       </div>
     </div>
   </div>
