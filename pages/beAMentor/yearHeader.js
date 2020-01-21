1cccd7c30211ee6a75011a46b281852e30864491
Lara Salameh <hello@larasalameh.com>
2020-01-21 16:50:35 +1100

    updated env assetsurl and beamentor 2020

--- pages/beAMentor/yearHeader.js
+++ pages/beAMentor/yearHeader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Title from 'aime-blueprint/lib/components/title';
+import styles from './beAMentor.module.scss';
 
 const YearHeader = () => (
   <div className="year-header sm-col sm-col-12 clearfix center">
-    <Title type="headingLockup" theme={process.env.REACT_APP_THEME}>
+    <Title type="headingLockup" className={styles.yearWelcomeHeading} theme={process.env.REACT_APP_THEME}>
       {`Bring on ${new Date().getFullYear()},`}
       <strong>
         Change is
