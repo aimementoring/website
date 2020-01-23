f53e330f1cd2071f6b5421547e3c3eceaa26c9e5
Lara Salameh <hello@larasalameh.com>
2020-01-23 17:10:22 +1100

    Grammar update

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -121,7 +121,7 @@ const FAQs = ({ faqsData, categories }) => {
                 {`, thank you for wanting to know about AIME. We’ll do our best
                 to front foot some of the common questions. If we don’t nail them all, feel free
                 to `}
-                <IntercomChat label="ping us a note" />
+                <IntercomChat label="Ping us a note" />
                 {`. Your questions help us refine our
                 answers and we'll use them to update this space and help everyone to understand
                 AIME as quickly as possible.`}
