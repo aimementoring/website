8e6e0b61a53f73a0fd7dfe4fbd121b6f3075908f
Kevin Bardi <bardi.kevin@gmail.com>
2020-01-28 23:43:25 -0300

    Clean up project

--- components/layoutComponents/header/imaginationDeclarationHeader.js
+++ components/layoutComponents/header/imaginationDeclarationHeader.js
@@ -4,7 +4,7 @@ import Anchor from '../../common/link';
 import MyContext from '../../../layouts/context';
 import styles from './header.module.scss';
 
-const Logo = dynamic(() => import(/* webpackChunkName 'Logo' */ './logo'));
+const Logo = dynamic(() => import('./logo'));
 
 const ImaginationDeclarationHeader = () => {
   const { imaginationDeclarationNumber } = useContext(MyContext);
