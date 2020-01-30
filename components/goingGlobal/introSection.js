9d4cd1a1ab71211b2336be9fd375e4b4b65d32e4
Mischa Colley <me@mischacolley.com.au>
2020-01-30 16:26:56 +1100

    refactor going-global to use componenets dir

--- components/goingGlobal/introSection.js
+++ components/goingGlobal/introSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './goingGlobal.module.scss';
+import styles from './goingGlobalIndex.module.scss';
 
 const Intro = ({ setReference }) => (
   <section
