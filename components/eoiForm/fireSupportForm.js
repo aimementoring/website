bb17e3344d402fe1ef52f31ba95fef73f2686fab
Kevin Bardi <bardi.kevin@gmail.com>
2020-01-31 10:55:22 -0300

    Fixes some errors on fire-support

--- components/eoiForm/fireSupportForm.js
+++ components/eoiForm/fireSupportForm.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropType from 'prop-types';
-import Input from '../commonElements/Input';
+import Router from 'next/router';
+import Input from 'aime-blueprint/lib/components/input';
+import Textarea from 'aime-blueprint/lib/components/textarea';
+import Loading from 'aime-blueprint/lib/components/loading';
+import PhoneInput from 'aime-blueprint/lib/components/phoneInput';
 import UniversitySelector from '../universitySelectorWithAutoComplete';
-import TextAreaInput from '../commonElements/TextAreaInput';
-import PhoneInput from '../commonElements/PhoneInput';
-import Loading from '../commonElements/Loading';
 import { getUniversities } from '../../services/portalApi';
 import './EOIForm.scss';
 
-export default class EOIForm extends Component {
-  state = {
+const tableName = 'Fire Support';
+
+const EOIForm = ({ currentSite, uploadData }) => {
+  const [state, setState] = useState({
     showOtherSourceConditional: false,
     phone: '',
     firstName: '',
@@ -24,208 +27,204 @@ export default class EOIForm extends Component {
     beAnAimeFriend: false,
     universityOptions: [],
     universityOutsideOfAime: '',
+  });
+
+  const formEl = useRef(null);
+
+  const fetchUniversities = async () => {
+    const universities = await getUniversities();
+    const universityOptions = universities
+      .filter((university) => university.eoi !== null && university.eoi !== undefined
+        && university.country === 'Australia')
+      .map((university) => ({
+        value: university.eoi,
+        label: university.name,
+        country: university.country,
+        reportingName: university.reportingName && university.reportingName.toLowerCase(),
+      }));
+
+    setState({ ...state, universityOptions });
   };
 
-  componentDidMount() {
-    getUniversities()
-      .then(universities => {
-        const universityOptions = universities
-          .filter(university => university.eoi !== null && university.eoi !== undefined &&
-            university.country === 'Australia')
-          .map(university => {
-            return {
-              value: university.eoi,
-              label: university.name,
-              country: university.country,
-              reportingName: university.reportingName && university.reportingName.toLowerCase(),
-            }
-          });
-        this.setState({ universityOptions })
-      });
-  }
+  useEffect(() => {
+    fetchUniversities();
+  });
 
-  handleUniversityChange = (field, value) => {
+  const handleUniversityChange = (field, value) => {
+    let universityValues = { university: value, universityIsOutsideAIME: false };
     if (value === 'I can’t find my university/college here') {
-      this.setState({ university: '', universityIsOutsideAIME: true });
-    } else {
-      this.setState({
-        university: value,
-        universityIsOutsideAIME: false,
-      });
+      universityValues = { university: '', universityIsOutsideAIME: true };
     }
-  }
-
-  handleFieldChange = (propertyName, propertyValue) => {
-    this.setState({ [propertyName]: propertyValue });
+    setState({ ...state, universityValues });
   };
 
-  handlePhoneCountrySelected = () => {
+  const handleFieldChange = (propertyName, propertyValue) => {
+    setState({ ...state, [propertyName]: propertyValue });
+  };
+
+  const handlePhoneCountrySelected = () => {
     const phoneCountrySelected = document.getElementsByClassName('flag-container')[0]
       .firstElementChild.title;
     if (phoneCountrySelected) {
       const phoneArea = phoneCountrySelected.split('+')
         ? `+${phoneCountrySelected.split('+')[1]}`
         : '';
-      this.setState({ phoneCountrySelected: phoneArea });
+      setState({ ...state, phoneCountrySelected: phoneArea });
       return phoneArea;
     }
     return '';
   };
 
-  submitData = e => {
-    if (!this.formEl.checkValidity()) {
-      return;
-    }
+  const submitData = async (e) => {
+    if (!formEl.checkValidity()) return;
+
     e.preventDefault();
-    const { tableName, uploadData, history } = this.props;
     const {
       phoneCountrySelected,
       university,
       universityOutsideOfAime,
-    } = this.state;
+    } = state;
     if (!university && !universityOutsideOfAime) {
-      this.setState({ submitClicked: true });
+      setState({ ...state, submitClicked: true });
       return;
     }
-    this.setState({ submitClicked: true, loading: true }, () => {
+    setState({ ...state, submitClicked: true, loading: true }, async () => {
       const objectToSend = {
-        ...this.state,
+        ...state,
         tableName,
       };
       if (!phoneCountrySelected) {
-        objectToSend.phoneCountrySelected = this.handlePhoneCountrySelected();
+        objectToSend.phoneCountrySelected = handlePhoneCountrySelected();
       }
-      uploadData(objectToSend).then(response => {
-        this.setState({ loading: false });
-        if (response.data === 'OK') {
-          history.push({
-            pathname: '/thanks',
-            messages: {
-              thankMessage: `
-                Thank you for offering your support.
-                We'll be in touch with next steps ASAP.
-                `,
-            },
+      const response = await uploadData(objectToSend);
+      setState({ ...state, loading: false });
+      if (response.data === 'OK') {
+        Router.push({
+          pathname: '/thanks',
+          query: {
+            messages: `
+              Thank you for offering your support.
+              We'll be in touch with next steps ASAP.
+              `,
             imageUrl: {
               imgUrlPath: '/assets/images/thanks/thumbsup@2x.png',
             },
-          });
-          return;
-        }
-        throw new Error('Error saving the EOI');
-      });
+          },
+        });
+        return;
+      }
+      throw new Error('Error saving the EOI');
     });
   };
 
-  render() {
-    const {
-      phone,
-      firstName,
-      lastName,
-      email,
-      university,
-      universityOptions,
-      universityOutsideOfAime,
-      universityIsOutsideAIME,
-      message,
-      loading,
-      submitClicked,
-    } = this.state;
-    const { currentSite } = this.props;
-    return (
-      <form className="clearfix form-light student-chapter-form" ref={form => (this.formEl = form)}>
-        <input type="hidden" name="utf8" />
-        <Input
-          placeholder="First Name"
-          name="firstName"
-          required
-          type="text"
-          elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
-          value={firstName}
-          onChangeFunction={this.handleFieldChange}
-        />
-        <Input
-          type="text"
-          elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
-          placeholder="Last Name"
-          name="lastName"
-          value={lastName}
-          onChangeFunction={this.handleFieldChange}
-          required
-        />
-        <Input
-          type="email"
-          elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
-          placeholder="Email"
-          name="email"
-          value={email}
-          onChangeFunction={this.handleFieldChange}
-          required
-        />
+  const {
+    phone,
+    firstName,
+    lastName,
+    email,
+    university,
+    universityOptions,
+    universityOutsideOfAime,
+    universityIsOutsideAIME,
+    message,
+    loading,
+    submitClicked,
+  } = state;
+
+  return (
+    <form className="clearfix form-light student-chapter-form" ref={formEl}>
+      <input type="hidden" name="utf8" />
+      <Input
+        placeholder="First Name"
+        name="firstName"
+        required
+        type="text"
+        elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
+        value={firstName}
+        onChangeFunction={handleFieldChange}
+      />
+      <Input
+        type="text"
+        elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
+        placeholder="Last Name"
+        name="lastName"
+        value={lastName}
+        onChangeFunction={handleFieldChange}
+        required
+      />
+      <Input
+        type="email"
+        elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChangeFunction={handleFieldChange}
+        required
+      />
+      <div>
         <div>
-          <div>
-            <PhoneInput
-              elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
-              onChangeFunction={this.handleFieldChange}
-              onCountrySelected={this.handlePhoneCountrySelected}
-              name="phone"
-              value={phone}
-              defaultCountry="au"
-              currentSite={currentSite}
-            />
-          </div>
-        </div>
-        <div className="sm-col sm-col-12 md-col-12 custom-form-input-container university-eoi-form-container">
-          <UniversitySelector
-            placeholder="Start typing to search for university/college"
-            onChangeFunction={this.handleUniversityChange}
-            name="university"
-            isClearable
-            error={submitClicked && !university}
-            containerClassNames="select-uni-location"
-            universityOptions={universityOptions}
-            value={university || null}
-            backgroundColor="#FFFF"
-            borderColor="#E3E5E5"
+          <PhoneInput
+            elementClassName="sm-col sm-col-6 md-col-6 custom-form-input-container"
+            onChangeFunction={handleFieldChange}
+            onCountrySelected={handlePhoneCountrySelected}
+            name="phone"
+            value={phone}
+            defaultCountry="au"
+            currentSite={currentSite}
           />
         </div>
-        {universityIsOutsideAIME && (
-          <Input
-            type="universityOutsideOfAime"
-            elementClassName="sm-col sm-col-12 md-col-12 custom-form-input-container"
-            placeholder="Your University"
-            name="universityOutsideOfAime"
-            className="mh-60"
-            value={universityOutsideOfAime}
-            onChangeFunction={this.handleFieldChange}
-            required
-          />
-        )}
-        <TextAreaInput
-          elementClassName="input-wrapper textarea-wrapper"
-          className="input-heigth"
-          placeholder="Please provide your location, dates, availability &amp; any specialist skills you have"
-          name="message"
-          value={message}
-          onChangeFunction={this.handleFieldChange}
+      </div>
+      <div className="sm-col sm-col-12 md-col-12 custom-form-input-container university-eoi-form-container">
+        <UniversitySelector
+          placeholder="Start typing to search for university/college"
+          onChangeFunction={handleUniversityChange}
+          name="university"
+          isClearable
+          error={submitClicked && !university}
+          containerClassNames="select-uni-location"
+          universityOptions={universityOptions}
+          value={university || null}
+          backgroundColor="#FFFF"
+          borderColor="#E3E5E5"
         />
-        <div className="sm-col sm-col-12 md-col-12 center-align md-left-align py2 my1">
-          <input onClick={this.submitData} type="submit" className="submit support-submit" value="REGISTER" />
-        </div>
-        <div>
-          <Loading loading={loading} />
-        </div>
-      </form>
-    );
-  }
-}
+      </div>
+      {universityIsOutsideAIME && (
+        <Input
+          type="universityOutsideOfAime"
+          elementClassName="sm-col sm-col-12 md-col-12 custom-form-input-container"
+          placeholder="Your University"
+          name="universityOutsideOfAime"
+          className="mh-60"
+          value={universityOutsideOfAime}
+          onChangeFunction={handleFieldChange}
+          required
+        />
+      )}
+      <Textarea
+        elementClassName="input-wrapper textarea-wrapper"
+        className="input-heigth"
+        placeholder="Please provide your location, dates, availability &amp; any specialist skills you have"
+        name="message"
+        value={message}
+        onChangeFunction={handleFieldChange}
+      />
+      <div className="sm-col sm-col-12 md-col-12 center-align md-left-align py2 my1">
+        <input onClick={submitData} type="submit" className="submit support-submit" value="REGISTER" />
+      </div>
+      <div>
+        <Loading loading={loading} />
+      </div>
+    </form>
+  );
+};
 
 EOIForm.propTypes = {
-  history: PropType.object.isRequired,
   uploadData: PropType.func.isRequired,
-  tableName: PropType.string,
+  currentSite: PropType.string,
 };
 
 EOIForm.defaultProps = {
-  tableName: '',
+  currentSite: 'au',
 };
+
+export default EOIForm;
--- pages/fire-support/index.js
+++ pages/fire-support/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { getAssetsBaseUrl } from '../../services/craftAPI';
 import { uploadCustomEOI } from '../../services/portalApi';
 import EoiForm from '../../components/eoiForm/fireSupportForm';
@@ -8,117 +9,111 @@ import styles from './styles.module.scss';
 const assetsUrl = getAssetsBaseUrl();
 const tableName = 'Fire Support';
 
-const FireSupport = ({ handleReloadData }) => {
-  // const { history } = this.props;
+const FireSupport = ({ handleReloadData }) => (
+  <Layout>
+    <div>
+      <div />
+      <div className={styles.microSite}>
+        <section className={styles.sectionWrapper}>
+          <div className={styles.heroPanelWrapper}>
+            <h1 className={styles.pageHeroHeader}>Mentor Fire Relief Support</h1>
+            <img
+              src={`${assetsUrl}/assets/images/fire-support/support-hands@2x.png`}
+              alt="Supporting Hands"
+              style={{ width: '550px' }}
+            />
+            {/* <h1 className={`${styles.pageHeroHeader} ${styles.mantra}`}>
+              Better Together
+            </h1> */}
+          </div>
+        </section>
 
-  return (
-    <Layout>
-      <div>
-        <div />
-        <div className={styles.microSite}>
-          <section className={styles.sectionWrapper}>
-            <div className={styles.heroPanelWrapper}>
-              <h1 className={styles.pageHeroHeader}>Mentor Fire Relief Support</h1>
-              <img
-                src={`${assetsUrl}/assets/images/fire-support/support-hands@2x.png`}
-                alt="Supporting Hands"
-                style={{ width: '550px' }}
-              />
-              {/* <h1 className={`${styles.pageHeroHeader} ${styles.mantra}`}>
-                Better Together
-              </h1> */}
-            </div>
-          </section>
+        <section className={styles.eoiSection}>
+          <div className={styles.formWrapper}>
+            <h4>
+              Brutal fires are raging across Australia. We are calling on you
+              AIME mentors, past and present, and anyone
+              in the AIME network. Please volunteer your time to
+              be a one-off AIME mentor,
+              from today until the end of March, to support the victims of fires
+              across our country.
+            </h4>
+          </div>
+        </section>
+        <section className={styles.imgSection}>
+          <div className={styles.maplocayfires}>
+            <p>
+              AIME works in schools and communities many of which have been ravaged
+              by fires up and down the eastern seaboard.
+            </p>
+            <img
+              src={`${assetsUrl}/assets/images/fire-support/firesaimemap@2x.png`}
+              alt="Fires and AIME locations map"
+            />
+          </div>
+        </section>
+        <section className={styles.eoiSection}>
+          <div>
+            <p>
+              The following areas are most in need of our support right now:
+            </p>
+            <p>
+              <strong>
+                Bega
+              </strong>
+              <br />
+              <strong>
+                Batemans Bay
+              </strong>
+              <br />
+              <strong>
+                Shoalhaven
+              </strong>
+              <br />
+              <strong>
+                Gippsland
+              </strong>
+              <br />
+              <strong>
+                Kalgoorlie
+              </strong>
+            </p>
+            <p>
+              Even if you are not located near one of these areas
+              we’ll work out how we can get you involved. Just
+              let us know your program site that you were or are
+              connected with (it will make things easier for our team to coordinate),
+              the dates you are free over the next two months,
+              and how many hours you can commit, and we will aim to connect you
+              with the organisations that need you.
+            </p>
+            <p>
+              We cannot ignore the realities of climate change and the effects it’s
+              having on our lives, no matter what the politics. The situation demands
+              more, and what leaders say and do today will define if they are
+              suitable for what’s to come.
+            </p>
+            <p>
+              <mark className={styles.textHighlight}>
+                We are better together.
+              </mark>
+            </p>
 
-          <section className={styles.eoiSection}>
-            <div className={styles.formWrapper}>
-              <h4>
-                Brutal fires are raging across Australia. We are calling on you
-                AIME mentors, past and present, and anyone
-                in the AIME network. Please volunteer your time to
-                be a one-off AIME mentor,
-                from today until the end of March, to support the victims of fires
-                across our country.
-              </h4>
-            </div>
-          </section>
-          <section className={styles.imgSection}>
-            <div className={styles.maplocayfires}>
-              <p>
-                AIME works in schools and communities many of which have been ravaged
-                by fires up and down the eastern seaboard.
-              </p>
-              <img
-                src={`${assetsUrl}/assets/images/fire-support/firesaimemap@2x.png`}
-                alt="Fires and AIME locations map"
-              />
-            </div>
-          </section>
-          <section className={styles.eoiSection}>
-            <div>
-              <p>
-                The following areas are most in need of our support right now:
-              </p>
-              <p>
-                <strong>
-                  Bega
-                </strong>
-                <br />
-                <strong>
-                  Batemans Bay
-                </strong>
-                <br />
-                <strong>
-                  Shoalhaven
-                </strong>
-                <br />
-                <strong>
-                  Gippsland
-                </strong>
-                <br />
-                <strong>
-                  Kalgoorlie
-                </strong>
-              </p>
-              <p>
-                Even if you are not located near one of these areas
-                we’ll work out how we can get you involved. Just
-                let us know your program site that you were or are
-                connected with (it will make things easier for our team to coordinate),
-                the dates you are free over the next two months,
-                and how many hours you can commit, and we will aim to connect you
-                with the organisations that need you.
-              </p>
-              <p>
-                We cannot ignore the realities of climate change and the effects it’s
-                having on our lives, no matter what the politics. The situation demands
-                more, and what leaders say and do today will define if they are
-                suitable for what’s to come.
-              </p>
-              <p>
-                <mark className={styles.textHighlight}>
-                  We are better together.
-                </mark>
-              </p>
-
-              <EoiForm
-                // history={history}
-                uploadData={uploadCustomEOI}
-                tableName={tableName}
-                showBeAFriendCheckbox
-                handleReloadData={handleReloadData}
-              />
-            </div>
-          </section>
-        </div>
+            <EoiForm
+              uploadData={uploadCustomEOI}
+              tableName={tableName}
+              showBeAFriendCheckbox
+              handleReloadData={handleReloadData}
+            />
+          </div>
+        </section>
       </div>
-    </Layout>
-    );
-  };
-  
-  Register.propTypes = {
-    handleReloadData: PropTypes.func.isRequired,
-  };
+    </div>
+  </Layout>
+);
 
-  export default FireSupport;
-  
+FireSupport.propTypes = {
+  handleReloadData: PropTypes.func.isRequired,
+};
+
+export default FireSupport;
