e19b2f2146de2c7cd1296f547d240e65199d5002
Kevin Bardi <bardi.kevin@gmail.com>
2020-01-30 21:30:29 -0300

    Remove env variable and adds ssl for expressjs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const cacheableResponse = require('cacheable-response');
 const express = require('express');
+const ssl = require('express-ssl');
 const next = require('next');
 const compression = require('compression');
 const fetchContentfulEntries = require('./api/contentfulRedirects');
@@ -24,6 +25,7 @@ const ssrCache = cacheableResponse({
 
 app.prepare().then(() => {
   const server = express();
+  server.use(ssl());
   server.use(compression());
 
   server.get('/', (req, res) => {
