7a3decd57214725fbaf085e1424dbb598c2c7b4a
Cristian Campos <camposcristian90@gmail.com>
2020-01-31 11:09:36 +1100

    testing https again

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ app.prepare().then(() => {
   const server = express();
   server.use(compression());
 
+  server.get('*', (req, res) => {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.REACT_APP_HOST_ENV !== 'development') {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    return handle(req, res);
+  });
+
   server.get('/', (req, res) => {
     ssrCache({ req, res, pagePath: '/' });
   });
