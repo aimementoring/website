ffc9722f367e7f2769db6ff23dfe3ed0369e37b8
bronz3beard <info2rory@gmail.com>
2020-01-31 13:14:49 +1000

    fix redirects take 2

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.prepare().then(() => {
   const server = express();
   server.use(compression());
 
+  server.get('*', (req, res) => {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.REACT_APP_HOST_ENV !== 'development') {
+      res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    return handle(req, res);
+  });
+
   server.get('/', (req, res) => {
     ssrCache({ req, res, pagePath: '/' });
   });
@@ -58,13 +65,6 @@ app.prepare().then(() => {
         res.redirect(url.fields.redirectType, url.fields.destinationUrl);
       });
     }
-
-    server.get('*', (req, res) => {
-      if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.REACT_APP_HOST_ENV !== 'development') {
-        res.redirect(`https://${req.headers.host}${req.url}`);
-      }
-      return handle(req, res);
-    });
 
     server.listen(port, (err) => {
       if (err) throw err;
