3fbe7a739494386300be816be88d4e88bb8fad01
Kevin Bardi <bardi.kevin@gmail.com>
2020-02-04 09:45:59 -0300

    Update styles from Lara PR

--- components/positions/backAction.js
+++ components/positions/backAction.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import Anchor from '../common/link';
 
 const BackAction = () => (
-  <Anchor
-    to="/positions#opportunity-list"
-    className="article-tile-link basic-btn italic"
-  >
-    <i className="material-icons">keyboard_backspace</i>
-      Back to
-      Opportunity List
-  </Anchor>
+  <Paragraph>
+    <Anchor to="/positions#opportunity-list" className="backToOpps">
+      <i className="material-icons">keyboard_backspace</i>
+      Back to list of opportunities
+    </Anchor>
+  </Paragraph>
 );
 
 export default BackAction;
