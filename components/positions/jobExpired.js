4be4c339a56d2709605a505c4f47db76436e10ec
Kevin Bardi <bardi.kevin@gmail.com>
2020-02-04 09:45:59 -0300

    Update styles from Lara PR

--- components/positions/jobExpired.js
+++ components/positions/jobExpired.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
 
 const JobExpired = ({ positionExpired }) => (
   <div>
     {positionExpired && (
       <div className="block mb3 md-mb4 lg-mb4 f-14 light js-unavailable-position c-black">
-        <p>Sadly this position has expired</p>
+        <Paragraph>Sadly this position has expired</Paragraph>
       </div>
     )}
   </div>
