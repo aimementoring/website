a4e8e7e3243ba300e0366c5aa7cc50803a6cdf11
Kevin Bardi <bardi.kevin@gmail.com>
2020-02-04 09:45:59 -0300

    Update styles from Lara PR

--- components/positions/jobApplyAction.js
+++ components/positions/jobApplyAction.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'aime-blueprint/lib/components/button';
 
 const JobApplyAction = ({ showForm, showApplicationForm, job }) => (
   <div>
     {!showForm && Object.keys(job).length > 0 && (
-      <button
+      <Button
+        theme={process.env.REACT_APP_THEME}
         id="applyNowButton"
         type="button"
-        onClick={showApplicationForm}
+        onClickFunction={showApplicationForm}
         className="basic-btn border-none submit bold bg-purple c-white regular js-non-unavailable-position"
         aria-label="apply now"
       >
         Apply now
-      </button>
+      </Button>
     )}
   </div>
 );
