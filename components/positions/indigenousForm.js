7f4fb25990c4eafa8caf3d370375bb41ba5b0607
Kevin Bardi <bardi.kevin@gmail.com>
2020-02-05 16:14:19 -0300

    Adds some fixes in the form

--- components/positions/indigenousForm.js
+++ components/positions/indigenousForm.js
@@ -2,49 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Checkbox from 'aime-blueprint/lib/components/checkbox';
 
-const IndigenousForm = ({ country }) => (
+const IndigenousForm = ({ handleChange, values }) => (
   <div>
-    {country && country
-      .toString()
-      .toLowerCase()
-      .indexOf('australia') > -1 && (
-      <div className="sm-col sm-col-12 md-col-12 f-14 pt1 pb1 flex items-center custom-checkbox custom-checkbox--default">
-        <input type="hidden" name="indigenous" value="no" />
-
-        <Checkbox
-          id="indigenous"
-          // onChangeFunction={updateValue}
-          placeholder="I identify as an Aboriginal and/or Torres Strait Islander person."
-          name="checkbox"
-          customId="checkbox"
-          // value={state.checkbox}
-          onChangeFunction={(name, updateValue) => setValue(updateValue)}
-          theme={process.env.REACT_APP_THEME}
-        />;
-        
-        {/* <input
-          id="indigenous"
-          type="checkbox"
-          className="hide"
-          name="indigenous"
-          value="yes"
-        /> */}
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        {/* <label htmlFor="indigenous">
-          I identify as an Aboriginal and/or Torres Strait
-          Islander person.
-        </label> */}
-      </div>
-    )}
+    <div className="sm-col sm-col-12 md-col-12 f-14 pt1 pb1 flex items-center custom-checkbox custom-checkbox--default">
+      <Checkbox
+        id="indigenous"
+        placeholder="I identify as an Aboriginal and/or Torres Strait Islander person."
+        name="indigenous"
+        value={values.indigenous || false}
+        onChangeFunction={handleChange}
+        theme={process.env.REACT_APP_THEME}
+      />
+    </div>
   </div>
 );
 
 IndigenousForm.propTypes = {
-  country: PropTypes.string,
+  handleChange: PropTypes.func,
+  values: PropTypes.shape({
+    indigenous: PropTypes.bool,
+  }),
 };
 
 IndigenousForm.defaultProps = {
-  country: '',
+  handleChange: () => {},
+  values: {},
 };
 
 export default IndigenousForm;
