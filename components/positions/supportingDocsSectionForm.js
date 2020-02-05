f5436c5382929129e70e62746871c32537ba6fa5
Lara Salameh <hello@larasalameh.com>
2020-02-05 14:12:32 +1100

    updating form components

--- components/positions/supportingDocsSectionForm.js
+++ components/positions/supportingDocsSectionForm.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Title from 'aime-blueprint/lib/components/title';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import kebabCase from 'lodash/kebabCase';
 import FileUploader from '../fileUploader';
 
 const SupportingDocsSectionForm = ({ requiredDocuments }) => (
   <div className="clearfix my3 sm-col sm-col-12 md-col-12 js-job-documents">
-    <h4 className="feature-font-family regular f-15 pb2">
+    <Title type="h5Title" theme={process.env.REACT_APP_THEME}>
       Provide Supporting Documents
-    </h4>
-    <p className="f-14 light pb2">
+    </Title>
+    <Paragraph>
       We need the below documents to proceed with your
       application.
-    </p>
+    </Paragraph>
     <div className="flex flex-column items-start job-documents js-job-document-container">
       {requiredDocuments
         && requiredDocuments.length > 0
