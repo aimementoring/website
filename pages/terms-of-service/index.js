e906960e3f5a43d4e2d31fac08fac4f69aaf5389
bronz3beard <info2rory@gmail.com>
2020-02-07 10:42:17 +1000

    Added contentful to terms of service

--- pages/terms-of-service/index.js
+++ pages/terms-of-service/index.js
@@ -1,36 +1,122 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Components } from 'aime-blueprint';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
+import PropTypes from 'prop-types';
+import contentfulServer from '../../api/contentfulStories';
 import Layout from '../../hocs/basicLayout';
-import { getEntries } from '../../services/craftAPI';
 
-const TermsAndConditions = () => {
-  const [paragraph, setParagraph] = useState('');
-  const [title, setTitle] = useState('');
+const {
+  Title,
+} = Components;
 
-  const fetchTermsAndConditions = async () => {
-    const data = await getEntries('terms');
-    setParagraph(data.data[0].generalPageMatrix[1].content.paragraph);
-    setTitle(data.data[0].title);
-  };
-
-  useEffect(() => {
-    fetchTermsAndConditions();
-  }, []);
+const TermsAndConditions = (props) => {
+  const { entries } = props;
 
   return (
     <Layout>
-      <div style={{ height: '117px', backgroundColor: 'black' }} />
-      <div className="matrix-general">
-        <div className="wrap-md mb0 md-mb3 lg-mb3 clearfix">
-          <span className="line above" />
-          <h1 className="above center c-black w100">{title}</h1>
-        </div>
-        <div className="wrap-md">
-          {/* eslint-disable-next-line react/no-danger */}
-          <div dangerouslySetInnerHTML={{ __html: paragraph }} />
-        </div>
-      </div>
+      <div style={{ height: `${120}px`, backgroundColor: 'black' }} />
+      {entries && entries.map((terms) => {
+        const termContent = terms.fields.contentCards
+          && terms.fields.contentCards.map((card) => (
+            <ReactMarkdown
+              key={card.sys.id}
+              source={card.fields.contentCopy}
+              renderers={{ paragraph: Paragraph }}
+            />
+          ));
+        return (
+          <div key={terms.sys.id} className="matrix-general">
+            <div className="wrap-md mb0 md-mb3 lg-mb3 clearfix">
+              <span className="line above" />
+              <Title type="h5Title">{terms.fields.title}</Title>
+            </div>
+            <div className="wrap-md">
+              {termContent}
+            </div>
+          </div>
+        );
+      })}
     </Layout>
   );
+};
+
+TermsAndConditions.getInitialProps = async () => {
+  const client = contentfulServer();
+  const entries = await client.then((response) => response);
+  const getReportsPosts = entries.filter((entry) => (entry.fields.contentTag.fields.name === 'terms'));
+
+  return { entries: getReportsPosts };
+};
+
+const SysShape = PropTypes.shape({
+  id: PropTypes.string,
+});
+
+TermsAndConditions.defaultProps = {
+  entries: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+TermsAndConditions.propTypes = {
+  entries: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    contentType: PropTypes.string,
+    contentTag: PropTypes.shape({
+      name: PropTypes.string,
+      sys: SysShape,
+    }),
+    banner: PropTypes.shape({
+      displayType: PropTypes.string,
+      heading: PropTypes.shape({
+        headingText: PropTypes.string,
+        type: PropTypes.string,
+        sys: SysShape,
+      }),
+      visualMedia: PropTypes.shape({
+        file: PropTypes.shape({
+          url: PropTypes.string,
+          fileName: PropTypes.string,
+          contentType: PropTypes.string,
+        }),
+        title: PropTypes.string,
+        sys: SysShape,
+      }),
+      sys: SysShape,
+    }),
+    contentCreator: PropTypes.shape({
+      authorName: PropTypes.string,
+      supportAuthorName: PropTypes.string,
+      sys: SysShape,
+    }),
+    publishDate: PropTypes.string,
+    seoAndMetaTags: PropTypes.shape({
+      platformMetaTags: PropTypes.arrayOf(PropTypes.shape({
+        sys: SysShape,
+      })),
+      sys: SysShape,
+    }),
+    contentPreview: PropTypes.shape({
+      displayType: PropTypes.string,
+      previewCopy: PropTypes.string,
+      title: PropTypes.string,
+      visualMedia: PropTypes.shape({
+        sys: SysShape,
+      }),
+      visualMediaCarousel: PropTypes.arrayOf(PropTypes.shape({
+        sys: SysShape,
+      })),
+    }),
+    contentCards: PropTypes.arrayOf(PropTypes.shape({
+      Type: PropTypes.string,
+      displayType: PropTypes.string,
+      visualMedia: PropTypes.shape({
+        sys: SysShape,
+      }),
+      videoMedia: PropTypes.string,
+      contentCopy: PropTypes.string,
+      sys: SysShape,
+    })),
+  })),
 };
 
 export default TermsAndConditions;
