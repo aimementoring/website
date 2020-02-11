9539c311f7fe4ca85cc415ff2b32b07fc7af4301
Lara Salameh <hello@larasalameh.com>
2020-02-11 15:21:16 +1100

    setting up quicklinks grid

--- components/quicklinksHomepage/index.js
+++ components/quicklinksHomepage/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import { Components } from 'aime-blueprint';
 import Paragraph from 'aime-blueprint/lib/components/paragraph';
-import './index.scss';
+import './style.scss';
 
-const VideoButton = dynamic(() => import('../videoButton'));
 
 const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 
@@ -14,15 +13,51 @@ const {
 } = Components;
 
 const QuicklinksHomepage = () => (
-  <div className="full-width-wrap content-panel home-intro-panel">
-    <div className="lg-wrap sm-col-12 md-col-10 mx-auto">
-      <div className="home-intro-panel--inner">
-        <div className="sm-col-12 sm-col-6 pr2 home-intro-panel-inner-content">
-          <Title type="h1Title">
-            Quicklinks here
-          </Title>
-          
-        </div>
+  <div className="quicklinksPanel">
+    <Title type="h3Title" className="quicklinksHeader">
+      Get involved
+    </Title>
+    <div className="quicklinksGrid">
+      <div className="quicklinkGridItem">
+        <a href="#" className="quicklinkImage">
+          <img
+            src={`${ASSETS_URL}/assets/images/illustrations/speakr@2x.png`}
+            alt="speaker"
+          />
+        </a>
+        <Paragraph>
+          <a href="#" className="quicklinkTitle">
+            Partner with us
+          </a>
+        </Paragraph>
+      </div>
+      
+      <div className="quicklinkGridItem">
+        <a href="#" className="quicklinkImage">
+          <img
+            src={`${ASSETS_URL}/assets/images/illustrations/speakr@2x.png`}
+            alt="speaker"
+          />
+        </a>
+        <Paragraph>
+          <a href="#" className="quicklinkTitle">
+            Partner with us
+          </a>
+        </Paragraph>
+      </div>
+
+      <div className="quicklinkGridItem">
+        <a href="#" className="quicklinkImage">
+          <img
+            src={`${ASSETS_URL}/assets/images/illustrations/speakr@2x.png`}
+            alt="speaker"
+          />
+        </a>
+        <Paragraph>
+          <a href="#" className="quicklinkTitle">
+            Partner with us
+          </a>
+        </Paragraph>
       </div>
     </div>
   </div>
