0ea15409d4d50a380d3629154e004fbe11a801cf
Mischa Colley <me@mischacolley.com.au>
2020-02-13 10:18:19 +1100

    added type to ambassadors and added hs to carousel

--- components/ambassadors/index.js
+++ components/ambassadors/index.js
@@ -15,13 +15,21 @@ const {
 
 const CAROUSEL_SLIDES = [
   {
+    image: styles.mentorMakhale,
+    title: 'Makhale Tshifaro',
+    type: 'Hooded Scholar',
+    paragraph: 'I know that one’s greatest weakness lies in giving up when they fail, but the most certain way to succeed is always to try one last time. This is why I joined AIME, because failure is the opportunity to begin again more intelligently and I want to encourage mentees to use the power of failure.',
+  },
+  {
     image: styles.mentorUzo,
     title: 'Uzo Aduba',
+    type: 'Ambassadors',
     paragraph: 'By standing together with dignity and hope for the future, AIME transforms lives.',
   },
   {
     image: styles.mentorDarcy,
     title: 'Darcy Sketcher',
+    type: 'Mentor',
     paragraph: `I was sick of reading the paper or hearing stories of inequality
     in our own backyard. I decided to get off my backside and do something about
     it. AIME encourages and empowers our mentees to create a fairer and a more
@@ -30,6 +38,7 @@ const CAROUSEL_SLIDES = [
   {
     image: styles.mentorPatrick,
     title: 'Patrick J. Adams',
+    type: 'Ambassadors',
     paragraph: `Beyond my family, no people have helped shape my life, my career and my view
       of the world more than the mentors I have been lucky enough to work, play and
       live with. I’m so excited to have found AIME and to do everything I can to
@@ -38,6 +47,7 @@ const CAROUSEL_SLIDES = [
   {
     image: styles.mentorJade,
     title: 'Jade Symons',
+    type: 'Program Manager',
     paragraph: `AIME and its values are something I really believe in. It’s radical,
     cultural-shifting work is reshaping both the psychological framework of
     Indigenous and non-Indigenous people. AIME’s work really makes you
@@ -46,6 +56,7 @@ const CAROUSEL_SLIDES = [
   {
     image: styles.mentorIan,
     title: 'Ian Thorpe',
+    type: 'Ambassadors',
     paragraph: `I believe in the work AIME does and I want to see it broaden to being a
       service globally. I want to see this happen because I believe in the young
       people of our future and the future they can create for the world.`,
@@ -70,7 +81,9 @@ const Ambassadors = () => (
   </div>
 );
 
-const AmbassadorSlide = ({ image, title, paragraph }) => (
+const AmbassadorSlide = ({
+  image, title, type, paragraph,
+}) => (
   <div className={styles.mentorSlideWrapper}>
     <div className={styles.mentorImageWrapper}>
       <Anchor
@@ -84,7 +97,7 @@ const AmbassadorSlide = ({ image, title, paragraph }) => (
       <div className={styles.mentorContentWrapper}>
         <div className={styles.titleContainer}>
           <Title type="h2Title" className={styles.ambassadorName} theme={process.env.REACT_APP_THEME}>{title}</Title>
-          <Title type="h5Title" theme={process.env.REACT_APP_THEME}>Ambassador</Title>
+          <Title type="h5Title" theme={process.env.REACT_APP_THEME}>{type}</Title>
           <Paragraph theme={process.env.REACT_APP_THEME}>
             {paragraph}
           </Paragraph>
@@ -100,6 +113,7 @@ const AmbassadorSlide = ({ image, title, paragraph }) => (
 AmbassadorSlide.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   paragraph: PropTypes.string.isRequired,
 };
 
