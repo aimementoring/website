20c168e7696561c221a3e483b2c9ebfff6634699
Lara Salameh <hello@larasalameh.com>
2020-02-12 17:33:47 +1100

    partnering panel

--- components/ctaGrid/index.js
+++ components/ctaGrid/index.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import Title from 'aime-blueprint/lib/components/title';
 import Button from 'aime-blueprint/lib/components/button';
 import Paragraph from 'aime-blueprint/lib/components/paragraph';
+import IntercomChat from '../../components/intercom';
 import styles from './ctaGrid.module.scss';
+
+const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 
 const CtaGrid = ({ elements }) => (
   <div className={styles.ctaGridContainer}>
@@ -34,6 +37,37 @@ const CtaGrid = ({ elements }) => (
           </div>
         </div>
       ))}
+    </div>
+    <div className={styles.fullWidthPanelWrapper}>
+      <div className={styles.partnerPanelWrap}>
+        <div className={styles.contentWrap}>
+          <img
+            src={`${ASSETS_URL}/assets/images/illustrations/speakr@2x.png`}
+            alt="speaker"
+            className={styles.partnerPanelIcon}
+          />
+          <Title className={styles.partnerTitle} type="headingLockup">
+            Become an AIME
+            <strong>
+              Partner
+            </strong>
+          </Title>
+          <Paragraph>
+            Do you work for or lead an organisation? Maybe you want to
+            partner with us? We are currently working through our
+            partnership options for 2020 and beyond - We’re open
+            to build partnerships in many areas, and have structures
+            in place for many already.
+          </Paragraph>
+          <Paragraph>
+            If you want to know more get in touch with Helen,
+            our Director of Partnering.
+          </Paragraph>
+          <div className={styles.intercomBtnWrap}>
+            <IntercomChat />
+          </div>
+        </div> 
+      </div>
     </div>
   </div>
 );
