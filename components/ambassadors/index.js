6587be341b9b1f6bb0bf10f99ca35514a105c201
Lara Salameh <hello@larasalameh.com>
2020-02-12 19:47:07 +1100

    adding real mentors

--- components/ambassadors/index.js
+++ components/ambassadors/index.js
@@ -20,12 +20,28 @@ const CAROUSEL_SLIDES = [
     paragraph: 'By standing together with dignity and hope for the future, AIME transforms lives.',
   },
   {
+    image: styles.mentorDarcy,
+    title: 'Darcy Sketcher',
+    paragraph: `I was sick of reading the paper or hearing stories of inequality
+    in our own backyard. I decided to get off my backside and do something about
+    it. AIME encourages and empowers our mentees to create a fairer and a more
+    loving world.`,
+  },
+  {
     image: styles.mentorPatrick,
     title: 'Patrick J. Adams',
     paragraph: `Beyond my family, no people have helped shape my life, my career and my view
       of the world more than the mentors I have been lucky enough to work, play and
       live with. I’m so excited to have found AIME and to do everything I can to
       serve the cause of mentorship all over the world.`,
+  },
+  {
+    image: styles.mentorJade,
+    title: 'Jade Symons',
+    paragraph: `AIME and its values are something I really believe in. It’s radical,
+    cultural-shifting work is reshaping both the psychological framework of
+    Indigenous and non-Indigenous people. AIME’s work really makes you
+    believe in humans and our collective capacity.`,
   },
   {
     image: styles.mentorIan,
