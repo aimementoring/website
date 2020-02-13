4d289cc08c901de118fac9450cd11ccc949bbe98
bronz3beard <info2rory@gmail.com>
2020-02-13 17:24:31 +1000

    Alternative to hash scrollTo WIP

--- components/quicklinksHomepage/index.js
+++ components/quicklinksHomepage/index.js
@@ -1,62 +1,80 @@
 import React, {
   useEffect,
+  useRef,
 } from 'react';
-import PropTypes from 'prop-types';
-import scrollToComponent from '../../utils/scrollToComponent';
-import { isClientSide } from '../../utils/utilities';
+// import Router from 'next/router';
+// import PropTypes from 'prop-types';
 import Title from 'aime-blueprint/lib/components/title';
 import Paragraph from 'aime-blueprint/lib/components/paragraph';
+// import scrollToComponent from '../../utils/scrollToComponent';
+import { isClientSide } from '../../utils/utilities';
 import './style.scss';
 
 const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 
-const QuicklinksHomepage = ({ setReference }) => {
+const QuicklinksHomepage = () => {
 // const QuicklinksHomepage = () => {
-  const sectionRefs = {};
+  // const sectionRefs = {};
+  const testingScrollRef = useRef(null);
+  // const testingAnotherScrollRef = useRef(null);
 
-  const hash = isClientSide() ? window.location.hash.replace('#', '') : '';
+  // const createDynamicRefs = () => {
+  //   const x = Object.keys(sectionRefs).find((section) => {
+  //   // eslint-disable-next-line no-param-reassign
+  //   // eslint-disable-next-line no-multi-assign
+  //     sectionRefs[section] = useRef();
+  //   });
+  //   return x;
+  // // };
+  // console.log('TCL: sectionRefs', sectionRefs);
+  // console.log('TCL: sectionRefs ', Object.keys(sectionRefs).find((section) => sectionRefs[section]));
+  // console.log('TCL: CtaGrid -> createDynamicRefs', createDynamicRefs);
+
+
+  // const setReference = (name) => {
+  //   sectionRefs[name] = useRef(null);
+  // };
+
+  // const hash = isClientSide() ? window.location.hash.replace('#', '') : '';
 
   const scrollHandler = () => {
+    window.scrollTo(0, testingScrollRef.current.offsetTop);
     if (isClientSide()) {
-      const overSection = Object.keys(sectionRefs).find((section) => {
-        const top = sectionRefs[section].offsetTop;
-        const bottom = top + sectionRefs[section].offsetHeight;
-        const scrollPosition = window.pageYOffset;
-        return top <= scrollPosition && bottom > scrollPosition;
-      });
-      if (overSection && overSection !== hash.replace('#', '').toLowerCase()) {
-        Router.push(
-          `/#${overSection}`,
-          `/#${overSection}`,
-          { shallow: true },
-        );
-      }
+      // const testOffset = document.body.getBoundingClientRect();
+      // const offSetTop = Math.abs(testOffset.top);
+      window.scrollTo(0, testingScrollRef.current.offsetTop); // <- need to adjust this offsettop hides "boo" under menu.
+      // if (overSection && overSection !== hash.replace('#', '').toLowerCase()) {
+      //   Router.push(
+      //     `/#${overSection}`,
+      //     `/#${overSection}`,
+      //     { shallow: true },
+      //   );
     }
   };
 
   useEffect(() => {
-    if (isClientSide()) window.addEventListener('scroll', scrollHandler, false);
-  }, []);
+    console.log('testingScrollRef -> ', testingScrollRef.current);
+  }, [testingScrollRef]);
 
-  useEffect(() => {
-    if (isClientSide() && hash && hash !== '') {
-      const section = hash.replace('#', '').toLowerCase();
-      window.removeEventListener('scroll', scrollHandler, false);
-      if (section in sectionRefs) {
-        // const scrollerOptions = { align: 'top' };
-        // if (section === 'welcome') scrollerOptions.offset = -50;
-        scrollToComponent(sectionRefs[section]);
-        setTimeout(() => {
-          window.addEventListener('scroll', scrollHandler, false);
-        }, 1000);
-        // .on('end', () => window.addEventListener('scroll', scrollHandler, false));
-      }
-    }
-    const cleanup = () => {
-      window.removeEventListener('scroll', scrollHandler, false);
-    };
-    return cleanup;
-  }, [hash]);
+  // useEffect(() => {
+  //   if (isClientSide() && hash && hash !== '') {
+  //     const section = hash.replace('#', '').toLowerCase();
+  //     window.removeEventListener('scroll', scrollHandler, false);
+  //     if (section in sectionRefs) {
+  //       // const scrollerOptions = { align: 'top' };
+  //       // if (section === 'welcome') scrollerOptions.offset = -50;
+  //       scrollToComponent(sectionRefs[section]);
+  //       setTimeout(() => {
+  //         window.addEventListener('scroll', scrollHandler, false);
+  //       }, 1000);
+  //       // .on('end', () => window.addEventListener('scroll', scrollHandler, false));
+  //     }
+  //   }
+  //   const cleanup = () => {
+  //     window.removeEventListener('scroll', scrollHandler, false);
+  //   };
+  //   return cleanup;
+  // }, [hash]);
 
 
   return (
@@ -64,7 +82,7 @@ const QuicklinksHomepage = ({ setReference }) => {
       <Title type="h3Title" className="quicklinksHeader">
         Get involved
       </Title>
-      <a href="/#testingscroll">Test CLICK THIS</a>
+      <button onClick={scrollHandler} type="button">Test CLICK THIS</button>
 
       <div className="quicklinksGrid">
         <div className="quicklinkGridItem">
@@ -80,7 +98,7 @@ const QuicklinksHomepage = ({ setReference }) => {
             </a>
           </Paragraph>
         </div>
-        
+
         <div className="quicklinkGridItem">
           <a href="https://shop.aimementoring.com/" className="quicklinkImage">
             <img
@@ -137,9 +155,9 @@ const QuicklinksHomepage = ({ setReference }) => {
           </Paragraph>
         </div>
       </div>
-      
+
       <section
-        ref={(el) => setReference('testingscroll', el)}
+        ref={testingScrollRef}
         className="testingscroll"
       >
         <h1>Boo</h1>
@@ -150,8 +168,5 @@ const QuicklinksHomepage = ({ setReference }) => {
   );
 };
 
-QuicklinksHomepage.propTypes = {
-  setReference: PropTypes.func.isRequired,
-};
 
 export default QuicklinksHomepage;
