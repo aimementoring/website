32e18bda00a6a55c43941719ec2693d3d626c680
Lara Salameh <hello@larasalameh.com>
2020-02-18 14:29:37 +1100

    adding content & animation

--- components/quickNewsFeed/index.js
+++ components/quickNewsFeed/index.js
@@ -11,8 +11,16 @@ const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 
 const QuickNewsFeed = () => (
   <section className="quickNewsFeed">
-    <Title type="h5Title">
-      boop boop boop boop boop boop boop boop boop boop 
+    <Title type="h5Title" className="feedContent">
+      <span>
+        50 Word News - as of 18.02.20
+      </span>
+      Mentoring has begun in the USA - Apps are open.
+      Mentor Applications commenced in Aus and Africa.
+      New Pink Imagi-Nation Hoodie Available.
+      2020 Imagination Curriculum for every school in the world
+      partnerships are being formed. Rocket ship is launching
+      - jump on board. - JMB, AIME CEO & Founder
     </Title>
   </section>
 );
