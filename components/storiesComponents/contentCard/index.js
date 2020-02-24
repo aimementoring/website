0802b7e51046b3ed1cc988931a700f5b6a54f195
bronz3beard <info2rory@gmail.com>
2020-02-24 21:49:28 +1000

    refactor contentCard for new content model

--- components/storiesComponents/contentCard/index.js
+++ components/storiesComponents/contentCard/index.js
@@ -10,15 +10,17 @@ import VideoFormElement from '../../commonElements/videoFormElement';
 
 const Picture = dynamic(() => import('../../picture'));
 const CallToActionButton = dynamic(() => import('../callToActionButton'));
-const PostScriptMessage = dynamic(() => import('../postScriptMessage'));
 
 const ContentCard = (props) => {
   const {
-    author, signature, contentCards, buttonProps, postScriptContent, publishDate,
+    author, signature, contentCards, publishDate,
   } = props;
 
   const content = contentCards
   && contentCards.map((card) => {
+    const cardType = card.fields.Type;
+    const label = card.fields.contentTitle
+      && card.fields.contentTitle.fields.headingText;
     const isImage = card.fields.visualMedia && card.fields.visualMedia;
     const isVideo = card.fields.videoMedia && card.fields.videoMedia;
     const image = isImage && isImage.fields.file.url;
@@ -30,27 +32,32 @@ const ContentCard = (props) => {
 
     return (
       <Fragment key={card.sys.id}>
-        {isVideo ? (
-          <VideoFormElement
-            formElement={{ videoUrl: video, name: videoPlatform, autoplay: true }}
-          />
-        )
-          : isImage && (
-            <>
-              <Picture
-                image={{
-                  title,
-                  thumbnail: `https:${image}?q=50`,
-                  image: `https:${image}?fl=progressive`,
-                }}
+        {cardType !== 'Call To Action' && (
+          <>
+            {isVideo ? (
+              <VideoFormElement
+                formElement={{ videoUrl: video, name: videoPlatform, autoplay: true }}
               />
-              {imageCaption && <div className="figcaption">{imageCaption}</div>}
-            </>
-          )}
-        <ReactMarkdown
-          source={storyBody}
-          renderers={{ paragraph: Paragraph }}
-        />
+            )
+              : isImage && (
+                <>
+                  <Picture
+                    image={{
+                      title,
+                      thumbnail: `https:${image}?q=50`,
+                      image: `https:${image}?fl=progressive`,
+                    }}
+                  />
+                  {imageCaption && <div className="figcaption">{imageCaption}</div>}
+                </>
+              )}
+            <ReactMarkdown
+              source={storyBody}
+              renderers={{ paragraph: Paragraph }}
+            />
+          </>
+        )}
+        {cardType === 'Call To Action' && (<CallToActionButton label={label} externalUrl={storyBody} />)}
       </Fragment>
     );
   });
@@ -67,15 +74,6 @@ const ContentCard = (props) => {
             {formatDate(publishDate, 'long')}
           </strong>
         )}
-
-      {buttonProps && (
-        <CallToActionButton
-          buttonProps={buttonProps}
-        />
-      )}
-      {postScriptContent && (
-        <PostScriptMessage postScriptContent={postScriptContent} />
-      )}
     </div>
   );
 };
@@ -83,35 +81,6 @@ const ContentCard = (props) => {
 ContentCard.propTypes = {
   author: PropTypes.string,
   signature: PropTypes.string,
-  buttonProps: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-      externalUrl: PropTypes.string,
-    }),
-  ),
-  postScriptContent: PropTypes.arrayOf(
-    PropTypes.shape({
-      Type: PropTypes.string,
-      contentCopy: PropTypes.string,
-      displayType: PropTypes.string,
-      visualMedia: PropTypes.shape({
-        file: PropTypes.shape({
-          contentType: PropTypes.string,
-          fileName: PropTypes.string,
-          url: PropTypes.string,
-          title: PropTypes.string,
-          details: PropTypes.shape({
-            size: PropTypes.number,
-            image: PropTypes.shape({
-              height: PropTypes.number,
-              width: PropTypes.number,
-            }),
-          }),
-        }),
-      }),
-      visualMediaCarousel: PropTypes.arrayOf(PropTypes.shape({})),
-    }),
-  ),
   publishDate: PropTypes.string.isRequired,
   contentCards: PropTypes.arrayOf(
     PropTypes.shape({
@@ -141,8 +110,6 @@ ContentCard.propTypes = {
 ContentCard.defaultProps = {
   author: '',
   signature: '',
-  buttonProps: null,
-  postScriptContent: null,
 };
 
 export default ContentCard;
