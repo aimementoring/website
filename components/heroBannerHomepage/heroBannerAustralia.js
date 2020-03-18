21d04ed755d448e576505960b407560e17463098
Lara Salameh <hello@larasalameh.com>
2020-03-18 15:43:16 +1100

    WIP setting up new homepage banner

--- components/heroBannerHomepage/heroBannerAustralia.js
+++ components/heroBannerHomepage/heroBannerAustralia.js
@@ -5,26 +5,31 @@ import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import Button from 'aime-blueprint/lib/components/button';
 import styles from './heroBannerHomepage.module.scss';
 
+const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
+
 const HeroBannerAustralia = ({ scrollHandler }) => (
   <div className={styles.heroBannerHomepage}>
-    <div className={styles.heroBannerContainer}>
-      <div className={styles.bannerWrapper}>
-        <div className={styles.bannerContent}>
-          <Title className={styles.headingLockup} type="headingLockup" theme={process.env.REACT_APP_THEME}>
-            Welcome to
-            <strong>AIME</strong>
-          </Title>
-          <Paragraph>
-            We’re a global social movement of everyday humans, working
-            relentlessly to build a fairer world
-          </Paragraph>
-          <Button
-            theme={process.env.REACT_APP_THEME}
-            onClickFunction={scrollHandler}
-          >
-            Get involved
-          </Button>
-        </div>
+    <div className={styles.bannerWrapper}>
+      <div className={`${styles.bannerContent} ${styles.bannerItem}`}>
+        <Title className={styles.headingLockup} type="headingLockup" theme={process.env.REACT_APP_THEME}>
+          Introducing
+          <strong>Imagi-nation TV</strong>
+        </Title>
+        <Paragraph>
+          A chance for kids at home to have mentors in their lives to <em>Make Sense of Today & Imagine Tomorrow</em>. 
+        </Paragraph>
+        <Button
+          theme={process.env.REACT_APP_THEME}
+          onClickFunction={scrollHandler}
+        >
+          Watch now
+        </Button>
+      </div>
+      <div className={`${styles.bannerMediaFeature} ${styles.bannerItem}`}>
+        <img
+          src={`${ASSETS_URL}/assets/images/banner/heyhello@2x.jpg`}
+          alt="Welcome hello from AIME"
+        />
       </div>
     </div>
   </div>
