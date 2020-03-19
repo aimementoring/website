3a8daf869d7031766cc933f20dbe1a89c30d80fe
Rin Raeuber <mail@rin-raeuber.com>
2020-03-19 18:08:53 +0100

    Messy video player

--- pages/imagi-nation-tv/index.js
+++ pages/imagi-nation-tv/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import Layout from '../../hocs/basicLayout';
 import Title from 'aime-blueprint/lib/components/title';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
+import Button from 'aime-blueprint/lib/components/button';
+import Layout from '../../hocs/basicLayout';
 import CASE_STUDIES from '../../constants/caseStudiesList';
+import VideoPlayer from '../../components/videoPlayer';
 import styles from './styles.module.scss';
 
 const CaseStudyBox = dynamic(() => import('../../components/caseStudyBox'));
@@ -13,7 +16,11 @@ const ImagiNationTV = () => (
     {/* <div className="hero-banner--default hero-banner--case-studies full-width-wrap">
       <div className="flex flex-wrap items-center full-height">
         <div className="banner-wrapper">
-          <Title className={styles.headingLockup} type="headingLockup" theme={process.env.REACT_APP_THEME}>
+          <Title
+            className={styles.headingLockup}
+            type="headingLockup"
+            theme={process.env.REACT_APP_THEME}
+            >
             Welcome to
             <strong className={styles.hiddenTitle}>Imagi-Nation TV</strong>
           </Title>
@@ -33,7 +40,6 @@ const ImagiNationTV = () => (
               className={styles.logoImagiTV}
             />
           </Title>
-          
         </div>
       </div>
     </div>
@@ -41,18 +47,20 @@ const ImagiNationTV = () => (
     <div className={styles.contentWrapperCampaignPg}>
 
       <section className={styles.videoContainerFeature}>
-        <div className="video-container">
-          <iframe
-            title="case-studies-video-container"
-            src="https://player.vimeo.com/video/219339041?title=0&amp;byline=0&amp;portrait=0"
-            width="640"
-            height="360"
-            frameBorder="0"
-            webkitallowfullscreen=""
-            mozallowfullscreen=""
-            allowFullScreen=""
-          />
-        </div>
+        <VideoPlayer
+          url="https://www.youtube.com/watch?v=DiQaOlsDZlY"
+          imageUrl="https://images.ctfassets.net/iz0aikshgysc/4veRQTJ6ilhYLtfflt5yPu/514ecaff0ba258f1853b718ca70115bc/Sherice_banner.jpg"
+        >
+          <div>
+            <Title type="h4Title" theme={process.env.REACT_APP_THEME}>
+              Hey Lara
+            </Title>
+            <Paragraph theme={process.env.REACT_APP_THEME}>
+              Lara should click this video.
+            </Paragraph>
+            <Button type="link" text="Watch now" theme={process.env.REACT_APP_THEME} url="https://www.youtube.com/watch?v=DiQaOlsDZlY" />
+          </div>
+        </VideoPlayer>
       </section>
 
     </div>
