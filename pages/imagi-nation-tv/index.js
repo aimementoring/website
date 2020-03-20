92418a5b9f85a8261e54988cacf67111f8fd93b9
Lara Salameh <hello@larasalameh.com>
2020-03-20 16:25:38 +1100

    WIP video player

--- pages/imagi-nation-tv/index.js
+++ pages/imagi-nation-tv/index.js
@@ -13,23 +13,9 @@ const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 
 const ImagiNationTV = () => (
   <Layout>
-    {/* <div className="hero-banner--default hero-banner--case-studies full-width-wrap">
-      <div className="flex flex-wrap items-center full-height">
-        <div className="banner-wrapper">
-          <Title
-            className={styles.headingLockup}
-            type="headingLockup"
-            theme={process.env.REACT_APP_THEME}
-            >
-            Welcome to
-            <strong className={styles.hiddenTitle}>Imagi-Nation TV</strong>
-          </Title>
-        </div>
-      </div>
-    </div> */}
 
     <div className={styles.heroBannerImagiTV}>
-      <div className={styles.bannerWrapper}>
+      <div className={styles.subpageBannerWrapper}>
         <div className={styles.bannerContent}>
           <Title className={styles.welcomeTitle} type="h3Title" theme={process.env.REACT_APP_THEME}>
             Welcome to
@@ -48,10 +34,10 @@ const ImagiNationTV = () => (
 
       <section className={styles.videoContainerFeature}>
         <VideoPlayer
-          url="https://www.youtube.com/watch?v=DiQaOlsDZlY"
+          url="https://www.youtube.com/watch?v=Kcqz2ShknNw&feature=emb_logo"
           imageUrl="https://images.ctfassets.net/iz0aikshgysc/4veRQTJ6ilhYLtfflt5yPu/514ecaff0ba258f1853b718ca70115bc/Sherice_banner.jpg"
         >
-          <div>
+          <div className={styles.videoCaptionWrapper}>
             <Title type="h4Title" theme={process.env.REACT_APP_THEME}>
               Hey Lara
             </Title>
@@ -64,46 +50,7 @@ const ImagiNationTV = () => (
       </section>
 
     </div>
-
-    <section className="py3 relative">
-
-      <div className="wrap mx-auto">
-        <div className="mt0 pt1 mb3 flex items-center">
-          <span className="line bg-brand-tertiary inline-block mr2 md-mr3 lg-mr3" />
-          <div className="inline-block">
-            <p className="f-16 light c-grey block pt0 md-pt1 lg-pt1">
-              Award-winning journalist and human rights campaigner, Jeff McMullen got together
-              with a group of AIME mentors and alumni to chat about their experiences with the
-              program and what they gained from it.
-            </p>
-          </div>
-        </div>
-        <div className="video-container">
-          <iframe
-            title="case-studies-video-container"
-            src="https://player.vimeo.com/video/219339041?title=0&amp;byline=0&amp;portrait=0"
-            width="640"
-            height="360"
-            frameBorder="0"
-            webkitallowfullscreen=""
-            mozallowfullscreen=""
-            allowFullScreen=""
-          />
-        </div>
-
-        <div className="mb2 mt0 pt1 md-pt3 lg-pt3 md-mt4 lg-mt4 flex items-center">
-          <span className="line bg-brand-tertiary inline-block mr2 md-mr3 lg-mr3" />
-          <div className="inline-block">
-            <h1 className="inline-block lh-base">Case Studies</h1>
-            <p className="f-16 light c-grey block pt0 md-pt1 lg-pt1">THe AIME Classroom</p>
-          </div>
-        </div>
-
-        <div className="grid about-grid">
-          {CASE_STUDIES.map((caseStudy) => <CaseStudyBox {...caseStudy} key={caseStudy.name} />)}
-        </div>
-      </div>
-    </section>
+    
   </Layout>
 );
 
