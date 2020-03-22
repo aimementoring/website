22077a087eed1a518f005f8681b4c33d674e015c
Mischa Colley <me@mischacolley.com.au>
2020-03-23 09:26:22 +1100

    update language on homepage cta

--- constants/index.js
+++ constants/index.js
@@ -135,7 +135,7 @@ export const CTA_AU_HOMEPAGE = [
     title: 'About AIME',
     link: '/about',
     button: 'See what we\'re about',
-    description: 'We’re a global movement for change.',
+    description: 'We’re an Imagination Factory aiming to put a mentor in every kids life every day',
   },
   {
     title: 'Shop Apparel',
