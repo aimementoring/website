1eab5e1076d00b8bc78fe0ecc2199e1ad9cf97e2
Kevin Bardi <bardi.kevin@gmail.com>
2020-03-21 21:19:56 -0300

    Multiple fixes related to SEO and social sharing

--- hocs/basicLayout.js
+++ hocs/basicLayout.js
@@ -1,29 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'next/router';
 import PropTypes from 'prop-types';
-import SeoMetadata from '../components/layoutComponents/seoMetaData';
 import ErrorBoundary from '../components/common/errorBoundary';
 import Header from '../components/layoutComponents/header';
 import Footer from '../components/layoutComponents/footer';
 import Logos from '../components/layoutComponents/logos';
-import SEO_TAGS from '../constants/seoTags';
 import './basicLayout.scss';
 
 const Layout = ({ router, children }) => {
-  const [seo, setSeo] = useState({});
-
-  const addSeoAndRedirects = () => {
-    const { pathname } = router;
-
-    if (pathname in SEO_TAGS) {
-      setSeo(SEO_TAGS[pathname]);
-    }
-  };
-
-  useEffect(() => {
-    addSeoAndRedirects();
-  }, [router]);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [router.pathname]);
@@ -32,10 +16,7 @@ const Layout = ({ router, children }) => {
     <div>
       <ErrorBoundary>
         <Header location={router} />
-        <main role="main">
-          <SeoMetadata seo={seo} />
-          {children}
-        </main>
+        <main role="main">{children}</main>
         <Footer location={router} />
         <Logos />
         <div id="aime-parent-video-box" />
