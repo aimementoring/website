68bfb40aedda1dbfaf4009de0ab508daf8597c42
Lara Salameh <hello@larasalameh.com>
2020-03-23 11:45:41 +1100

    temp landing

--- pages/imagi-nation-tv/index.js
+++ pages/imagi-nation-tv/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import Title from 'aime-blueprint/lib/components/title';
 import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import Button from 'aime-blueprint/lib/components/button';
 import Layout from '../../hocs/basicLayout';
-import CASE_STUDIES from '../../constants/caseStudiesList';
-import VideoPlayer from '../../components/videoPlayer';
+// import CASE_STUDIES from '../../constants/caseStudiesList';
+// import VideoPlayer from '../../components/videoPlayer';
 import styles from './styles.module.scss';
 
-const CaseStudyBox = dynamic(() => import('../../components/caseStudyBox'));
+// const CaseStudyBox = dynamic(() => import('../../components/caseStudyBox'));
 const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 
 const ImagiNationTV = () => (
@@ -28,29 +28,43 @@ const ImagiNationTV = () => (
           </Title>
         </div>
       </div>
+      <div className={styles.videoContainerFeature}>
+        <a href="https://www.youtube.com/user/aimementoring/live" target="_blank">
+          <img
+            src={`${ASSETS_URL}/assets/images/illustrations/imagi-robot-soon@2x.gif`}
+            alt="Imagi-nation TV"
+            className={styles.videoCoverArt}
+          />
+        </a>
+      </div>
     </div>
+    <div className="imagi-landing-wrapper">
 
-    <div className={styles.contentWrapperCampaignPg}>
-
-      <section className={styles.videoContainerFeature}>
-        <VideoPlayer
-          url="https://www.youtube.com/watch?v=Kcqz2ShknNw&feature=emb_logo"
-          imageUrl="https://images.ctfassets.net/iz0aikshgysc/4veRQTJ6ilhYLtfflt5yPu/514ecaff0ba258f1853b718ca70115bc/Sherice_banner.jpg"
-        / >
-        <div className={styles.videoCaptionWrapper}>
-          <Title type="h4Title" theme={process.env.REACT_APP_THEME}>
-            IMAGINATION - EPISODE 1
-          </Title>
-          <Paragraph theme={process.env.REACT_APP_THEME}>
-            World class teachers and knowledge holders across our schools, universities and
-            further abroad, philosophers, and big picture thinkers distill the complexity of
-            today to help us imagine and create tomorrow in a Q&A format.
-          </Paragraph>
-          <Button type="link" text="Watch now" theme={process.env.REACT_APP_THEME} url="https://www.youtube.com/watch?v=DiQaOlsDZlY" />
-        </div>
-      </section>
-
+      <Paragraph className={styles.imagiPara}>
+        A chance for kids at home to have mentors in their lives to <em>Make Sense of Today & Imagine Tomorrow</em>. 
+      </Paragraph>
+      <div className="triBtnSet">
+        <Button type="link"
+          text="Watch the episode"
+          theme={process.env.REACT_APP_THEME}
+          className={`${styles.imagiBtn}`}
+          url="https://www.youtube.com/user/aimementoring/live" />
+        <Button type="link"
+          className={`${styles.imagiDonate} ${styles.imagiBtn}`}
+          text="Donate to [INTV]"
+          theme={process.env.REACT_APP_THEME}
+          url="https://imagi-nation-tv.raisely.com/" />
+        <Button type="link"
+          className={`${styles.imagiHoodieBtn} ${styles.imagiBtn}`}
+          text="Buy the Imagi-Nation Hoodie"
+          theme={process.env.REACT_APP_THEME}
+          url="https://shop.aimementoring.com/collections/all-products/products/imagi-nation-hoodie" />
+      </div>
     </div>
+    {/* <div className={styles.contentWrapperCampaignPg}>
+
+      
+    </div> */}
 
   </Layout>
 );
