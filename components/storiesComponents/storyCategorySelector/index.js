f6968d82056946d0bcaf6befdee533b2e64debe5
Rin Raeuber <mail@rin-raeuber.com>
2020-03-26 13:46:20 +0100

    Feature: Filter posts by category.

---
+++ components/storiesComponents/storyCategorySelector/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import dynamic from 'next/dynamic';
+import styles from './storyCategorySelector.module.scss';
+
+const Checkbox = dynamic(() => import('aime-blueprint/lib/components/checkbox'));
+
+const StoryCategorySelector = ({ categories, selectedCategories, onChangeFunction }) => (
+  <ul className={styles.storyCategorySelectorContainer}>
+    {categories.map((category) => (
+      <li key={category}>
+        <Checkbox
+          // elementClassName=""
+          // className=""
+          onChangeFunction={onChangeFunction}
+          name={category}
+          placeholder={category}
+          value={selectedCategories.includes(category)}
+          theme={process.env.REACT_APP_THEME}
+        />
+      </li>
+    ))}
+  </ul>
+);
+
+StoryCategorySelector.defaultProps = {
+  categories: [],
+  selectedCategories: [],
+  onChangeFunction: () => {},
+};
+
+StoryCategorySelector.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.string),
+  selectedCategories: PropTypes.arrayOf(PropTypes.string),
+  onChangeFunction: PropTypes.func,
+};
+
+export default StoryCategorySelector;
