46dd880c7bbb6cc9fc2d809d25d47cacfea08d16
Rin Raeuber <mail@rin-raeuber.com>
2020-03-26 13:46:20 +0100

    Feature: Filter posts by category.

--- components/storiesComponents/storiesGrid/index.js
+++ components/storiesComponents/storiesGrid/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Title from 'aime-blueprint/lib/components/title';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
 import StoriesContent from '../storiesContent';
 import entriesType from '../entriesType';
 import {
@@ -8,26 +10,37 @@ import {
 import styles from './storiesGrid.module.scss';
 
 const StoriesGrid = ({ entries }) => (
-  <div className={styles.storiesGrid}>
-    {entries.length > 0 && entries.map(({
-      fields: {
-        title, contentCreator, contentPreview, banner, publishDate, contentCards,
-      },
-      sys: { id },
-    }) => {
-      const storiesContentProps = {
-        id,
-        title,
-        slugTitle: replaceWhiteSpace(removeSpecialCharacters(title), '-').toLowerCase(),
-        contentCreator: contentCreator.fields.authorName,
-        contentPreview: contentPreview && contentPreview.fields,
-        bannerContent: banner && banner.fields,
-        publishDate,
-        contentCards,
-      };
-      return <StoriesContent {...storiesContentProps} key={storiesContentProps.id} />;
-    })}
-  </div>
+  <>
+    {entries.length === 0 && (
+      <div>
+        <Title type="h4Title" className={styles.pageIntroHeading} theme={process.env.REACT_APP_THEME}>Ooops!</Title>
+        <Paragraph>
+          {'We can\'t find any stories for the criteria you selected.'}
+        </Paragraph>
+      </div>
+    )}
+    <div className={styles.storiesGrid}>
+      {entries.length > 0 && entries.map(({
+        fields: {
+          title, contentCreator, postCategories, contentPreview, banner, publishDate, contentCards,
+        },
+        sys: { id },
+      }) => {
+        const storiesContentProps = {
+          id,
+          title,
+          slugTitle: replaceWhiteSpace(removeSpecialCharacters(title), '-').toLowerCase(),
+          contentCreator: contentCreator.fields.authorName,
+          contentPreview: contentPreview && contentPreview.fields,
+          bannerContent: banner && banner.fields,
+          categories: postCategories,
+          publishDate,
+          contentCards,
+        };
+        return <StoriesContent {...storiesContentProps} key={storiesContentProps.id} />;
+      })}
+    </div>
+  </>
 );
 
 StoriesGrid.propTypes = { entries: entriesType };
