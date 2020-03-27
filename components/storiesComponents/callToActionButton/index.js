611b0444cf30f8c5d2cb8156393a87f16db9b7a1
bronz3beard <info2rory@gmail.com>
2020-03-27 20:37:08 +1000

    updated to help migrate contentful content

--- components/storiesComponents/callToActionButton/index.js
+++ components/storiesComponents/callToActionButton/index.js
@@ -3,31 +3,65 @@ import PropTypes from 'prop-types';
 import styles from './CallToActionButton.module.scss';
 
 const CallToActionButton = (props) => {
-  const { label, externalUrl } = props;
+/*
+TODO: remove all reference to buttonProps once Contentful content model
+for post is updated for all stories.
+*/
+  const { label, externalUrl, buttonProps } = props;
 
   return (
     <div className={styles.buttonContainer}>
-      <a
-        href={externalUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          border: '0',
-          alignSelf: 'center',
-        }}
-        className={styles.postElementLink}
-      >
-        {label}
-      </a>
+      {!buttonProps
+        ? (
+
+          <a
+            href={externalUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              border: '0',
+              alignSelf: 'center',
+            }}
+            className={styles.postElementLink}
+          >
+            {label}
+          </a>
+        ) : (
+          <>
+            {/* TODO: remove once Contentful content model for post is updated for all stories. */}
+            {
+              buttonProps.map(({ fields, sys }) => (
+                <a
+                  key={sys.id}
+                  href={fields.externalUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    border: '0',
+                    alignSelf: 'center',
+                  }}
+                  className={styles.postElementLink}
+                >
+                  {fields.label}
+                </a>
+              ))
+
+            }
+          </>
+        )}
     </div>
   );
+};
+/* TODO: remove once Contentful content model for post is updated for all stories. */
+CallToActionButton.defaultProps = {
+  buttonProps: [],
 };
 
 CallToActionButton.propTypes = {
   label: PropTypes.string.isRequired,
   externalUrl: PropTypes.string.isRequired,
+  /* TODO: remove once Contentful content model for post is updated for all stories. */
+  buttonProps: PropTypes.arrayOf({}),
 };
-
-// CallToActionButton.defaultProps = {};
 
 export default CallToActionButton;
