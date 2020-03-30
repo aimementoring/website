82dd48e6e59616fdc2a015ffbda806ef76d55d8b
Kevin Bardi <bardi.kevin@gmail.com>
2020-03-30 19:51:35 -0300

    fix: Simplify hook

--- hooks/useDonate.js
+++ hooks/useDonate.js
@@ -1,16 +1,9 @@
-// useStore.js
-import { useState, useEffect, useContext } from 'react';
-import { ReactReduxContext } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleDonateModal } from '../store/donation/actions';
 
 const useDonate = () => {
-  const { store: { getState, dispatch, subscribe } } = useContext(ReactReduxContext);
-  const [donateModalState, setDonateModalState] = useState(getState().donation.donateModal);
-
-  // subscribe only once
-  useEffect(() => subscribe(
-    () => setDonateModalState(getState().donation.donateModal),
-  ));
+  const donateModalState = useSelector((state) => state.donation.donateModal);
+  const dispatch = useDispatch();
 
   return [donateModalState, () => dispatch(toggleDonateModal())];
 };
