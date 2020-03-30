d883af08c728df1b1fa2c6099c69eae489c82d60
Kevin Bardi <bardi.kevin@gmail.com>
2020-03-30 19:47:40 -0300

    fix: some fixes after review

--- store/donation/actions.js
+++ store/donation/actions.js
@@ -1,22 +1,21 @@
-/* eslint-disable import/prefer-default-export */
-import { GLOBAL_SET_DONATE_VISIBILITY, GLOBAL_TOGGLE_DONATE_VISIBILITY } from '../constants';
+import { DONATION_SET_DONATE_VISIBILITY, DONATION_TOGGLE_DONATE_VISIBILITY } from '../constants';
 
 export const showDonateModal = (visible) => (dispatch) => {
   dispatch({
-    type: GLOBAL_SET_DONATE_VISIBILITY,
+    type: DONATION_SET_DONATE_VISIBILITY,
     payload: visible,
   });
 };
 
 export const hideDonateModal = (visible) => (dispatch) => {
   dispatch({
-    type: GLOBAL_SET_DONATE_VISIBILITY,
+    type: DONATION_SET_DONATE_VISIBILITY,
     payload: visible,
   });
 };
 
 export const toggleDonateModal = () => (dispatch) => {
   dispatch({
-    type: GLOBAL_TOGGLE_DONATE_VISIBILITY,
+    type: DONATION_TOGGLE_DONATE_VISIBILITY,
   });
 };
