edcdbc0bc8cd2fc467e126da0f34f620410c1781
Rin Raeuber <mail@rin-raeuber.com>
2020-04-27 17:07:19 +0200

    Feature: Add typeform modal.

---
+++ components/typeformModal/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from '../modal';
+import styles from './typeformModal.module.scss';
+
+const TYPEFORM_URL = 'https://aimementoring.typeform.com/to/C9g3Pw';
+
+const TypeformModal = ({ visible, toggleModal }) => (
+  <Modal
+    showModal={visible}
+    handleModal={toggleModal}
+    hideBodyOverflowY
+    backgroundColor="rgba(0,0,0, 0.6)"
+    withCloseIcon
+  >
+    <div className={styles.typeformIframeContainer}>
+      <iframe
+        title="Typeform"
+        src={TYPEFORM_URL}
+        width="100%"
+        height="512"
+        border="0"
+        className={styles.typeformIframe}
+      />
+    </div>
+  </Modal>
+);
+
+TypeformModal.defaultProps = {
+  visible: false,
+};
+
+TypeformModal.propTypes = {
+  visible: PropTypes.bool,
+  toggleModal: PropTypes.func.isRequired,
+};
+
+export default TypeformModal;
