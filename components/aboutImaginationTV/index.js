a6c3cd94d6a3126be3c215d24ca165f391000937
Rin Raeuber <mail@rin-raeuber.com>
2020-04-29 13:31:48 +0200

    Feature: Add cyclops.

---
+++ components/aboutImaginationTV/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Title from 'aime-blueprint/lib/components/title';
+import Paragraph from 'aime-blueprint/lib/components/paragraph';
+import Button from 'aime-blueprint/lib/components/button';
+import IntercomChat from '../intercom';
+import styles from './aboutImaginationTV.module.scss';
+
+const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
+
+const AboutImaginationTv = () => (
+  <section className={styles.aboutInTVSection}>
+    <div className={styles.aboutInTVWrapper}>
+      <div className={styles.aboutParagraphColumn}>
+        <Title type="h3Title" className={styles.aboutInTVTitle}>
+          A mentor in the home for every kid, every day.
+        </Title>
+        <Paragraph>
+          AIME was founded in 2005 to bring mentors into the lives of kids left behind.
+          Since then we’ve grown to become global experts in mentoring - last year
+          being awarded one of the top 50 education groups in the world.
+        </Paragraph>
+        <Paragraph>
+          {'We created IMAGI-NATION{TV} & the IMAGI-NATION{CLASSROOM} experience to put a mentor in the home every day during the tough times of COVID-19 and beyond. It’s a daily TV show broadcast live on the Internet, and it’s a gift for teachers, parents and kids to help make sense of today & imagine tomorrow.'}
+        </Paragraph>
+        <div className={styles.becomeAPartnerBtn}>
+          <IntercomChat label="Partner with us" />
+        </div>
+      </div>
+      <div className={styles.aboutCyclopsColumn}>
+        <div className={styles.cyclopsSpeechBubble}>
+          <div>
+            <Button type="text" className={styles.speechButtonContent} onClickFunction={() => {}}>
+              Read the full
+              story
+              <br />
+              <span>here</span>
+            </Button>
+
+          </div>
+          <img
+            src={`${ASSETS_URL}/assets/images/illustrations/kyle-cyclops@2x.png`}
+            alt="Kyle the Cyclops"
+            className={styles.cyclopsImage}
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default AboutImaginationTv;
