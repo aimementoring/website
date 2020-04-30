51eb58b2b0eae3ced32393bea1d57deb1d092468
Rin Raeuber <mail@rin-raeuber.com>
2020-04-30 16:16:31 +0200

    Make scrolling work.

--- utils/scrollToComponent.js
+++ utils/scrollToComponent.js
@@ -1,3 +1,14 @@
-const scrollToComponent = (ref) => ref.scrollIntoView(true);
+import isClientSide from './isClientSide';
+
+const scrollToComponent = (ref) => {
+  if (isClientSide()) {
+    const isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style;
+    if (isSmoothScrollSupported) {
+      ref.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    } else {
+      ref.current.scrollIntoView(false);
+    }
+  }
+};
 
 export default scrollToComponent;
