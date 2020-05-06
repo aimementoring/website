47bbeb5283b3002941484b4e5adbeb06c03fe93f
charliemckenzie <cmk@aimementoring.com>
2020-05-06 15:02:10 +1000

    removed current nav - missed this originally

--- components/layoutComponents/header/standardHeader.js
+++ components/layoutComponents/header/standardHeader.js
@@ -1,58 +1,19 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import Anchor from '../../common/link';
-import { HEADER_MENU_ITEMS } from '../../../constants';
 import styles from './header.module.scss';
-import IntercomChat from '../../intercom';
-import useDonate from '../../../hooks/useDonate';
 
 const Logo = dynamic(() => import('./logo'));
-const MenuItem = dynamic(() => import('./menuItem'));
 const FullScreenMenu = dynamic(() => import('../fullScreenMenu'));
 
-const StandardHeader = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [modalVisible, toggleDonateModal] = useDonate();
-  return (
-    <div>
-      <div className={styles.siteHeaderContainer}>
-        <div className={styles.logoContainer}>
-          <Logo />
-        </div>
-        <nav id="nav" className={styles.navMenuLinks}>
-          <ul className={styles.list}>
-            {HEADER_MENU_ITEMS.map((item) => (
-              <MenuItem key={item.title.replace(/\s/g, '-')} {...item} />
-            ))}
-            <li className={styles.listItem}>
-              <Anchor className={styles.navBtn} to="/positions">
-                Work with us
-              </Anchor>
-            </li>
-            <li className={styles.listItem}>
-              <IntercomChat />
-            </li>
-            <li className={styles.inlineListItem}>
-              <a
-                className={styles.navBtn}
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://shop.aimementoring.com/"
-              >
-                Shop
-              </a>
-            </li>
-            <li className={styles.inlineListItem}>
-              <div className={styles.navBtn} onClick={toggleDonateModal} onKeyPress={toggleDonateModal} role="presentation">
-                Donate
-              </div>
-            </li>
-          </ul>
-        </nav>
-        <FullScreenMenu />
+const StandardHeader = () => (
+  <div>
+    <div className={styles.siteHeaderContainer}>
+      <div className={styles.logoContainer}>
+        <Logo />
       </div>
+      <FullScreenMenu />
     </div>
-  );
-};
+  </div>
+);
 
 export default StandardHeader;
