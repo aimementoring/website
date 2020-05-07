7ef14f6ee3f8487ec6092e0a5e734a3b6036ff05
Lara Salameh <hello@larasalameh.com>
2020-05-07 16:45:57 +1000

    adding new campaign constants

--- constants/index.js
+++ constants/index.js
@@ -126,6 +126,33 @@ export const CTA_AU_HOMEPAGE = [
   },
 ];
 
+export const CTA_GRID_CAMPAIGNS = [
+  {
+    title: 'IMAGINE FILM',
+    link: '/imagine',
+    button: 'See what it\'s about',
+    description: 'We\'re creating a feature film, with you!',
+  },
+  {
+    title: 'This Hoodie Pays Rent',
+    link: 'https://shop.aimementoring.com',
+    button: 'Grab some gear',
+    description: 'The home of the hoodie that changed the world',
+  },
+  {
+    title: 'Partner with us',
+    link: '/imagi-nation-tv',
+    button: 'Let\'s change the world',
+    description: 'Want to change the world? It\'s time to walk the talk!',
+  },
+  {
+    title: 'Work with us',
+    link: '/positions',
+    button: 'Make change!',
+    description: 'Get involved with us!',
+  },
+];
+
 export const CTA_US_HOMEPAGE = [
   {
     title: 'College Students',
