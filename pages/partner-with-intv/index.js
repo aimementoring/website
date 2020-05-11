0421415811077b2dd48afd838c29dbc770188467
Lara Salameh <hello@larasalameh.com>
2020-05-11 14:49:17 +1000

    herobann

--- pages/partner-with-intv/index.js
+++ pages/partner-with-intv/index.js
@@ -27,11 +27,11 @@ const PartnerWithIntv = () => {
 
   return (
     <Layout>
-      <div className={styles.heroBannerImagiTV}>
+      <div className={styles.heroBannerImagine}>
         <div className={styles.bannerWrapper}>
           <div className={`${styles.bannerContent} ${styles.bannerItem}`}>
             <Title className={styles.welcomeTitle} type="headingLockup" theme={process.env.REACT_APP_THEME}>
-              Partner with INTV
+              Partnering with
               <strong className={styles.hiddenTitle}>Imagi-Nation TV</strong>
               <img
                 src={`${ASSETS_URL}/assets/images/logos/logo_imagitv@2x.png`}
@@ -39,42 +39,15 @@ const PartnerWithIntv = () => {
                 className={styles.logoImagiTV}
               />
             </Title>
-            <Paragraph>
-              <strong><mark>&nbsp;Live every weekday 12pm AEST&nbsp;</mark></strong>
-              <br />
-              <br />
-              A chance for kids at home to have mentors in their lives to
-              {' '}
-              <em>Make Sense of Today & Imagine Tomorrow</em>
-            </Paragraph>
-
-            <Button
-              theme={process.env.REACT_APP_THEME}
-              className={`${styles.watchBtn}`}
-              url="https://www.youtube.com/user/aimementoring/live"
-              target="_blank"
-              type="link"
-            >
-              Watch live
-            </Button>
+           
           </div>
 
           <div className={`${styles.bannerMediaFeature} ${styles.bannerItem} ${styles.welcomeVideoWrapper}`}>
-            <VideoPlayer
-              url="https://player.vimeo.com/external/410482407.m3u8?s=ba7787f5d791c00de586ebbe81529c8ad01b835f"
-              imageUrl={`${ASSETS_URL}/assets/images/illustrations/intv-hosts@2x.jpg`}
-            >
-              <Paragraph className="videoCaption">
-                <a
-                  className={styles.textLinkChannel}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.youtube.com/channel/UCDL9R_msvYDyHF7lx0NEyow?sub_confirmation=1"
-                >
-                Subscribe to our channel
-                </a>
-              </Paragraph>
-            </VideoPlayer>
+            <img
+              src={`${ASSETS_URL}/assets/images/illustrations/cat@2x.png`}
+              alt="Robo Cat"
+              className={styles.smlPuppetTalking}
+            />
           </div>
 
         </div>
