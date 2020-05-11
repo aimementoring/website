25870d5433b087942db00e69c500d0fd0405a7c6
Lara Salameh <hello@larasalameh.com>
2020-05-11 16:11:41 +1000

    setting up panel wip

--- pages/partner-with-intv/index.js
+++ pages/partner-with-intv/index.js
@@ -10,7 +10,7 @@ import WavyDonateSection from '../../components/wavyDonateSection';
 import AboutImaginationTV from '../../components/aboutImaginationTV';
 import TypeformModal from '../../components/typeformModal';
 import IntercomChat from '../../components/intercom';
-import scrollToComponent from '../../utils/scrollToComponent';
+// import scrollToComponent from '../../utils/scrollToComponent';
 import styles from './partner-with-intv.module.scss';
 
 const DoubleCurvedLine = dynamic(() => import('../../components/imaginationTv/doubleCurvedLine'));
@@ -20,10 +20,10 @@ const ASSETS_URL = process.env.REACT_APP_ASSETS_URL;
 const SHOP_PRODUCT_LINK = process.env.REACT_APP_SHOP_PRODUCT_LINK;
 
 const PartnerWithIntv = () => {
-  const scrollToThisRef = useRef(null);
+  // const scrollToThisRef = useRef(null);
   const [showModal, setModal] = useState(false);
   const toggleModal = () => setModal(!showModal);
-  const scrollToPanel = () => scrollToComponent(scrollToThisRef);
+  // const scrollToPanel = () => scrollToComponent(scrollToThisRef);
 
   return (
     <Layout>
@@ -52,8 +52,47 @@ const PartnerWithIntv = () => {
 
         </div>
       </div>
-      <AboutImaginationTV scrollHandler={scrollToPanel} />
-      <div className={styles.inTVContentWrapper}>
+      {/* <AboutImaginationTV scrollHandler={scrollToPanel} /> */}
+
+      {/* name this two-column layout */}
+      <section className={styles.aboutInTVSection}>
+        <div className={styles.aboutInTVWrapper}>
+          <div className={styles.aboutParagraphColumn}>
+            <Title type="h3Title" className={styles.aboutInTVTitle}>
+              A mentor in the home for every kid, every day.
+            </Title>
+            <Paragraph>
+              AIME was founded in 2005 to bring mentors into the lives of kids left behind.
+              Since then we’ve grown to become global experts in mentoring - last year
+              being awarded one of the top 50 education groups in the world.
+            </Paragraph>
+            <Paragraph>
+              {'We created IMAGI-NATION{TV} & the IMAGI-NATION{CLASSROOM} experience to put a mentor in the home every day during the tough times of COVID-19 and beyond. It’s a daily TV show broadcast live on the Internet, and it’s a gift for teachers, parents and kids to help make sense of today & imagine tomorrow.'}
+            </Paragraph>
+            <div className={styles.becomeAPartnerBtn}>
+              <Button theme={process.env.REACT_APP_THEME}>
+                Partner with  us
+              </Button>
+            </div>
+          </div>
+          <div className={styles.aboutCyclopsColumn}>
+            <div className={styles.cyclopsSpeechBubble}>
+              <div>
+                <Title type="h5Title" className={styles.speechButtonContent}>
+                  Come join us &amp; watch the show!
+                </Title>
+              </div>
+              <img
+                src={`${ASSETS_URL}/assets/images/illustrations/kyle-cyclops@2x.png`}
+                alt="Kyle the Cyclops"
+                className={styles.cyclopsImage}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className={styles.intvPartneringContentWrapper}>
         <section className={styles.inTVEpisodesWrapper}>
           <ImaginationTvCarousel />
         </section>
@@ -69,7 +108,7 @@ const PartnerWithIntv = () => {
           />
         </section>
         <DoubleCurvedLine />
-        <section ref={scrollToThisRef} className={styles.partneringCTASection}>
+        <section className={styles.partneringCTASection}>
           <Title type="h3Title" className={styles.partneringHeading}>{'PARTNERING WITH IN{TV}'}</Title>
           <div className={styles.puppetsChatWrapper}>
             <TypeformModal visible={showModal} toggleModal={toggleModal} />
