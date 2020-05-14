943601c74b4c6f83b1fdd5d71ad9d95881d2f9ee
Rin Raeuber <mail@rin-raeuber.com>
2020-05-14 18:06:27 +0200

    Test: Test if this works.

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-console */
 const { exec } = require('child_process');
 const sitemap = require('nextjs-sitemap-generator');
+const fs = require('fs');
+require('dotenv').config();
+const fetchContentfulEntries = require('../api/contentfulRedirects');
 
 const AWS_ENVIRONMENTS = ['staging', 'production'];
+const NOW_CONFIG = 'now.json';
 
 async function goToRootFolder() {
   return new Promise((resolve, reject) => {
@@ -10,6 +14,22 @@ async function goToRootFolder() {
       if (error) reject(error);
       if (stderr) console.info(stderr);
       resolve(stdout);
+    });
+  });
+}
+
+async function addRedirectsToNowJson() {
+  return new Promise((resolve, reject) => {
+    fetchContentfulEntries().then((response = []) => {
+      const redirects = response.map((url) => ({
+        src: url.fields.sourceUrl,
+        dest: url.fields.destinationUrl,
+        status: url.fields.redirectType,
+      }));
+      const nowJson = JSON.parse(fs.readFileSync(NOW_CONFIG, 'utf8'));
+      nowJson.routes.push(...redirects);
+      fs.writeFile(NOW_CONFIG, JSON.stringify(nowJson), (err) => reject(err));
+      resolve(redirects);
     });
   });
 }
@@ -38,14 +58,16 @@ async function uploadToAWS(buildEnv) {
 async function runBuild() {
   const buildEnv = process.env.REACT_APP_HOST_ENV;
   const goesToAws = AWS_ENVIRONMENTS.indexOf(buildEnv) > -1;
-  const totalActions = goesToAws ? 6 : 4;
+  const totalActions = goesToAws ? 7 : 5;
   console.info('\x1b[32m%s\x1b[0m', `• 1 of ${totalActions} -> BUILD PROCESS STARTED`);
   try {
     console.info('\x1b[32m%s\x1b[0m', `• 2 of ${totalActions} -> POSITIONING IN ROOT FOLDER`);
     await goToRootFolder();
+    console.info('\x1b[32m%s\x1b[0m', `• 3 of ${totalActions} -> ADDING REDIRECTS`);
+    await addRedirectsToNowJson();
     if (goesToAws) {
       try {
-        console.info('\x1b[32m%s\x1b[0m', `• 3 of ${totalActions} -> GENERATING SITEMAP`);
+        console.info('\x1b[32m%s\x1b[0m', `• 4 of ${totalActions} -> GENERATING SITEMAP`);
         sitemap({
           baseUrl: 'https://aimementoring.com',
           pagesDirectory: 'pages',
@@ -56,14 +78,14 @@ async function runBuild() {
       }
     }
     try {
-      console.info('\x1b[32m%s\x1b[0m', `• ${goesToAws ? 4 : 3} of ${totalActions} -> BUILDING PROJECT`);
+      console.info('\x1b[32m%s\x1b[0m', `• ${goesToAws ? 5 : 4} of ${totalActions} -> BUILDING PROJECT`);
       const buildStdout = await buildProject();
       console.log(buildStdout);
       console.log('\x1b[36m%s\x1b[0m', `ENVIRONMENT: ${buildEnv}`);
       // exec('cp .next/service-worker.js public/service-worker.js');
       if (goesToAws) {
         try {
-          console.info('\x1b[32m%s\x1b[0m', `• ${goesToAws ? 5 : 4} of ${totalActions} -> UPLOADING BUILD FOLDER TO AWS S3`);
+          console.info('\x1b[32m%s\x1b[0m', `• ${goesToAws ? 6 : 5} of ${totalActions} -> UPLOADING BUILD FOLDER TO AWS S3`);
           const awsStdout = await uploadToAWS(buildEnv);
           console.log(awsStdout);
         } catch (error) {
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cacheableResponse = require('cacheable-response');
 const express = require('express');
 const next = require('next');
 const compression = require('compression');
-const fetchContentfulEntries = require('./api/contentfulRedirects');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -33,40 +32,29 @@ app.prepare().then(() => {
   //   app.serveStatic(req, res, filePath);
   // });
 
-  fetchContentfulEntries().then((response = []) => {
-    for (let i = 0; i < response.length; i += 1) {
-      const url = response[i];
-      if (url.fields.sourceUrl !== '/donate') {
-        server.get(url.fields.sourceUrl, (_req, res) => {
-          res.redirect(url.fields.redirectType, url.fields.destinationUrl);
-        });
-      }
+  server.get('/contact', (req, res) => {
+    ssrCache({ req, res, pagePath: '/' });
+  });
+
+  server.get('*', (req, res) => {
+    if (req.path.substr(-1) === '/' && req.path.length > 1) {
+      const query = req.url.slice(req.path.length);
+      res.redirect(301, `${req.path.slice(0, -1)}${query}`);
     }
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.REACT_APP_HOST_ENV !== 'development') {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    return handle(req, res);
+  });
 
-    server.get('/contact', (req, res) => {
-      ssrCache({ req, res, pagePath: '/' });
-    });
+  server.get('/', (req, res) => {
+    ssrCache({ req, res, pagePath: '/' });
+  });
 
-    server.get('*', (req, res) => {
-      if (req.path.substr(-1) === '/' && req.path.length > 1) {
-        const query = req.url.slice(req.path.length);
-        res.redirect(301, `${req.path.slice(0, -1)}${query}`);
-      }
-      if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.REACT_APP_HOST_ENV !== 'development') {
-        res.redirect(301, `https://${req.headers.host}${req.url}`);
-      }
-      return handle(req, res);
-    });
+  server.listen(port, (err) => {
+    if (err) throw err;
 
-    server.get('/', (req, res) => {
-      ssrCache({ req, res, pagePath: '/' });
-    });
-
-    server.listen(port, (err) => {
-      if (err) throw err;
-
-      // eslint-disable-next-line no-console
-      console.log(`> Ready on http://localhost:${port}`);
-    });
+    // eslint-disable-next-line no-console
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
