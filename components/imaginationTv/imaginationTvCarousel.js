18f03cf25afcb1687251579c04a455d3a99521c2
Kevin Bardi <kevinbardi@Kevins-MacBook-Pro.local>
2020-05-21 18:42:16 -0300

    fixes carousel slides

--- components/imaginationTv/imaginationTvCarousel.js
+++ components/imaginationTv/imaginationTvCarousel.js
@@ -15,9 +15,9 @@ const ImaginationTvCarousel = () => (
       color="lightblue"
     >
       <Paragraph>
-            World-class teachers and knowledge holders, across our schools and
-            universities and further abroad; the philosophers & big picture thinkers
-            of the world.
+        World-class teachers and knowledge holders, across our schools and
+        universities and further abroad; the philosophers & big picture thinkers
+        of the world.
       </Paragraph>
     </ImaginationTvCard>
     <ImaginationTvCard
@@ -27,30 +27,26 @@ const ImaginationTvCarousel = () => (
       color="aquamarine"
     >
       <Paragraph>
-        <strong>
-              Presidential Speech for IMAGI-NATION
-        </strong>
+        <strong>Presidential Speech for IMAGI-NATION</strong>
       </Paragraph>
       <Paragraph>
-            Every week, students from across the world are given the stage to deliver a
-            3-minute speech as the President of IMAGI-NATION. They will focus on the topic
-            of the week, and use the space to inspire us all to make sense of the world
-            today so we can imagine the world tomorrow.
+        Every week, students from across the world are given the stage to deliver a
+        3-minute speech as the President of IMAGI-NATION. They will focus on the topic
+        of the week, and use the space to inspire us all to make sense of the world
+        today so we can imagine the world tomorrow.
       </Paragraph>
       <Paragraph>
-        <strong>
-              CHAOS Classroom
-        </strong>
+        <strong>CHAOS Classroom</strong>
       </Paragraph>
       <Paragraph>
-            Join AIME Founder & CEO Jack Manning Bancroft and students from across
-            Australia and around the world who will take part in a 7 minute hyperdrive
-            lesson on the topic theme of the week. See a real life case study as to how
-            to run a classroom digitally amongst the chaos of today.
+        Join AIME Founder & CEO Jack Manning Bancroft and students from across
+        Australia and around the world who will take part in a 7 minute hyperdrive
+        lesson on the topic theme of the week. See a real life case study as to how
+        to run a classroom digitally amongst the chaos of today.
       </Paragraph>
       <Paragraph>
-            We’ll also be inviting boss human guest teachers take on the reins of
-            the CHAOS Classroom and run a session on any one of our 21 core values.
+        We’ll also be inviting boss human guest teachers take on the reins of
+        the CHAOS Classroom and run a session on any one of our 21 core values.
       </Paragraph>
     </ImaginationTvCard>
     <ImaginationTvCard
@@ -60,54 +56,15 @@ const ImaginationTvCarousel = () => (
       color="orange"
     >
       <Paragraph>
-            Those building the world today and tomorrow.
-            There’s no limit to what we believe is “design”.
+        Those building the world today and tomorrow.
+        There’s no limit to what we believe is “design”.
       </Paragraph>
       <Paragraph>
-            You’ll have the chance to dance around a big picture topic with 2
-            other panelists. We are elevating the knowledge of designers who have
-            got us here today, who have created the world we live in. We are
-            exploring the mindsets, processes, philosophy and thinking patterns
-            that have taken you to where you are now.
-      </Paragraph>
-    </ImaginationTvCard>
-    <ImaginationTvCard
-      day="Thursday"
-      title="Keynote"
-      image="keynote_tablet"
-      color="purple"
-    >
-      <Paragraph>
-        <strong>
-              &quot;Cancelled, not cancelled&quot;
-        </strong>
-      </Paragraph>
-      <Paragraph>
-            Each week AIME gives the stage to a festival or conference that has been
-            cancelled to have some level of publicity and promotion.
-      </Paragraph>
-      <Paragraph>
-            To inspire marginalised kids to rise up and change their world.
-            To inspire us all to seek knowledge, to find inspiration in ideas, and
-            embrace the kindness of someone sharing what they know with us.
-      </Paragraph>
-    </ImaginationTvCard>
-    <ImaginationTvCard
-      day="Wednesday"
-      title="Designers"
-      image="designers_tv"
-      color="orange"
-    >
-      <Paragraph>
-            Those building the world today and tomorrow.
-            There’s no limit to what we believe is “design”.
-      </Paragraph>
-      <Paragraph>
-            You’ll have the chance to dance around a big picture topic with 2
-            other panelists. We are elevating the knowledge of designers who have
-            got us here today, who have created the world we live in. We are
-            exploring the mindsets, processes, philosophy and thinking patterns
-            that have taken you to where you are now.
+        You’ll have the chance to dance around a big picture topic with 2
+        other panelists. We are elevating the knowledge of designers who have
+        got us here today, who have created the world we live in. We are
+        exploring the mindsets, processes, philosophy and thinking patterns
+        that have taken you to where you are now.
       </Paragraph>
     </ImaginationTvCard>
     <ImaginationTvCard
@@ -117,18 +74,16 @@ const ImaginationTvCarousel = () => (
       color="purple"
     >
       <Paragraph>
-        <strong>
-              &quot;Cancelled, not cancelled&quot;
-        </strong>
+        <strong>&quot;Cancelled, not cancelled&quot;</strong>
       </Paragraph>
       <Paragraph>
-            Each week AIME gives the stage to a festival or conference that has been
-            cancelled to have some level of publicity and promotion.
+        Each week AIME gives the stage to a festival or conference that has been
+        cancelled to have some level of publicity and promotion.
       </Paragraph>
       <Paragraph>
-            To inspire marginalised kids to rise up and change their world.
-            To inspire us all to seek knowledge, to find inspiration in ideas, and
-            embrace the kindness of someone sharing what they know with us.
+        To inspire marginalised kids to rise up and change their world.
+        To inspire us all to seek knowledge, to find inspiration in ideas, and
+        embrace the kindness of someone sharing what they know with us.
       </Paragraph>
     </ImaginationTvCard>
     <ImaginationTvCard
