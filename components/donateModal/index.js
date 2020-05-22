5764316fd29d9417f38a2d278f26b5a5a10812cb
Cristian Campos <camposcristianeze@users.noreply.github.com>
2020-05-21 22:23:51 -0400

    Testing static deploy for most pages (#495)
    * Removing unused banner. Making it static
    
    * updating node version
    
    * freaking camelcase....
    
    * Commenting donate modal using redux to test static deploys
    
    * Adding redirects to static
    
    * Make header static
    
    * now is vercel now?
    
    * testing with slashes?
    
    * testing with less routes
    
    * testing menu and header as static
    
    * Menu+redirects
    
    * Fixes Donate modal and some settings
    
    Co-authored-by: Kevin Bardi <kevinbardi@Kevins-MacBook-Pro.local>

--- components/donateModal/index.js
+++ components/donateModal/index.js
@@ -1,42 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react';
 import Modal from '../modal';
-import { toggleDonateModal } from '../../store/donation/actions';
+import DonationContext from '../../context';
 import styles from './donateModal.module.scss';
 
-const DonateModal = ({ visible, toggleModal }) => (
-  <Modal
-    showModal={visible}
-    handleModal={toggleModal}
-    hideBodyOverflowY
-    backgroundColor="rgba(0,0,0, 0.6)"
-    withCloseIcon
-  >
-    <div className={styles.donateIframeContainer}>
-      <iframe
-        title="donate"
-        src="https://aimedonations.raisely.com/embed/"
-        width="100%"
-        height="512"
-        border="0"
-        className={styles.donateIframe}
-      />
-    </div>
-  </Modal>
-);
-
-const mapStateToProps = (states) => ({
-  visible: states.donation.donateModal,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  toggleModal: () => dispatch(toggleDonateModal()),
-});
-
-DonateModal.propTypes = {
-  visible: PropTypes.bool.isRequired,
-  toggleModal: PropTypes.func.isRequired,
+const DonateModal = () => {
+  const { donationModal, toggleDonationModal } = useContext(DonationContext);
+  return (
+    <Modal
+      showModal={donationModal}
+      handleModal={toggleDonationModal}
+      hideBodyOverflowY
+      backgroundColor="rgba(0,0,0, 0.6)"
+      withCloseIcon
+    >
+      <div className={styles.donateIframeContainer}>
+        <iframe
+          title="donate"
+          src="https://aimedonations.raisely.com/embed/"
+          width="100%"
+          height="512"
+          border="0"
+          className={styles.donateIframe}
+        />
+      </div>
+    </Modal>
+  );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(DonateModal);
+export default DonateModal;
