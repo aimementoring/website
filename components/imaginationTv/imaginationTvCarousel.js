e1554ab526c9b40c2632779c302e892936b415aa
Lara Salameh <hello@larasalameh.com>
2020-05-25 13:56:31 +1000

    adding daily playlist btn on carousel

--- components/imaginationTv/imaginationTvCarousel.js
+++ components/imaginationTv/imaginationTvCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import Paragraph from 'aime-blueprint/lib/components/paragraph';
+import Button from 'aime-blueprint/lib/components/button';
 import Carousel from '../carousel';
 import styles from './imaginationTvCarousel.module.scss';
 
@@ -19,6 +20,15 @@ const ImaginationTvCarousel = () => (
         thinkers distill the complexity of today to help us imagine
         and create tomorrow.
       </Paragraph>
+      <Button
+        theme={process.env.REACT_APP_THEME}
+        url="https://www.youtube.com/playlist?list=PLjfNcXcq0TOSJwVsb3_aKwhQPI9Cw0w-o"
+        target="_blank"
+        type="link"
+        containerClassName={styles.btnPlaylist}
+      >
+        Take a look
+      </Button>
     </ImaginationTvCard>
     <ImaginationTvCard
       day="Tuesday"
@@ -41,6 +51,17 @@ const ImaginationTvCarousel = () => (
         : Students take part in a virtual classroom where they
         try new things and learn through failure.
       </Paragraph>
+
+      <Button
+        theme={process.env.REACT_APP_THEME}
+        url="https://www.youtube.com/playlist?list=PLjfNcXcq0TOTLdpQpUi7bPpDD9jig94Q6"
+        target="_blank"
+        type="link"
+        containerClassName={styles.btnPlaylist}
+      >
+        Take a look
+      </Button>
+
     </ImaginationTvCard>
     <ImaginationTvCard
       day="Wednesday"
@@ -52,6 +73,17 @@ const ImaginationTvCarousel = () => (
         Those building the world today and tomorrow explore the mindsets, processes,
         philosophy and thinking patterns that help them create solutions.
       </Paragraph>
+
+      <Button
+        theme={process.env.REACT_APP_THEME}
+        url="https://www.youtube.com/playlist?list=PLjfNcXcq0TOQ2sI2h2FOyR3bCATFcsnBv"
+        target="_blank"
+        type="link"
+        containerClassName={styles.btnPlaylist}
+      >
+        Take a look
+      </Button>
+
     </ImaginationTvCard>
     <ImaginationTvCard
       day="Thursday"
@@ -63,6 +95,17 @@ const ImaginationTvCarousel = () => (
         Giving the stage to people scheduled to speak or perform at now-cancelled
         events to share their wisdom and knowledge with the world.
       </Paragraph>
+
+      <Button
+        theme={process.env.REACT_APP_THEME}
+        url="https://www.youtube.com/playlist?list=PLjfNcXcq0TOR8GKctCAnVKlAeXW3wn3lD"
+        target="_blank"
+        type="link"
+        containerClassName={styles.btnPlaylist}
+      >
+        Take a look
+      </Button>
+
     </ImaginationTvCard>
     <ImaginationTvCard
       day="Friday"
@@ -75,6 +118,17 @@ const ImaginationTvCarousel = () => (
         from blank space to work of art, to the accompanying sound of musicians
         with a message.
       </Paragraph>
+
+      <Button
+        theme={process.env.REACT_APP_THEME}
+        url="https://www.youtube.com/playlist?list=PLjfNcXcq0TOSQ7S96SmNa5GItQdNjY_9d"
+        target="_blank"
+        type="link"
+        containerClassName={styles.btnPlaylist}
+      >
+        Take a look
+      </Button>
+
     </ImaginationTvCard>
   </Carousel>
 );
